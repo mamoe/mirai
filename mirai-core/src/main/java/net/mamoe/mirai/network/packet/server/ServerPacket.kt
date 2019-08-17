fce4bbc86bc9882db552f3df2ec5e5cb66554775
liujiahua123123 <liujiahua123123@126.com>
2019-08-17 22:30:02 +0800

    fix

--- mirai-core/src/main/java/net/mamoe/mirai/network/packet/server/ServerPacket.kt
+++ mirai-core/src/main/java/net/mamoe/mirai/network/packet/server/ServerPacket.kt
@@ -54,7 +54,7 @@ fun DataInputStream.readIP(): String {
         val byte = readByte();
         buff += (byte.toUByte().toString())
         if(i !=3)buff+="."
-        System.out.println(byte.toHexString())
+        //System.out.println(byte.toHexString())
     }
     return buff
 }
--- mirai-core/src/test/java/NetworkTest.java
+++ mirai-core/src/test/java/NetworkTest.java
@@ -34,5 +34,4 @@ public class NetworkTest {
     }
 
 
-
 }
