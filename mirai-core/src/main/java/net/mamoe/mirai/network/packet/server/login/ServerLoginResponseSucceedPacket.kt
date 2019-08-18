d3064bc0887231978724af259857f9a4a783a3de
liujiahua123123 <liujiahua123123@126.com>
2019-08-18 17:28:32 +0800

    decode

--- mirai-core/src/main/java/net/mamoe/mirai/network/packet/server/login/ServerLoginResponseSucceedPacket.kt
+++ mirai-core/src/main/java/net/mamoe/mirai/network/packet/server/login/ServerLoginResponseSucceedPacket.kt
@@ -16,10 +16,10 @@ import java.io.DataInputStream
  */
 class ServerLoginResponseSucceedPacket(input: DataInputStream, val packetDataLength: Int) : ServerPacket(input) {
     lateinit var _0828_rec_decr_key: ByteArray//16 bytes|
-    var age: Int = 0
-    var gender: Boolean = false//from 1byte
+    var age: Short = 0
+    var gender: Int = 0//from 1byte
     lateinit var nick: String
-    lateinit var clientKey: String
+    lateinit var clientKey: ByteArray
 
     lateinit var token38: ByteArray
     lateinit var token88: ByteArray
@@ -46,11 +46,23 @@ class ServerLoginResponseSucceedPacket(input: DataInputStream, val packetDataLen
                 else -> throw IllegalStateException()
             }//144
 
-        this.input.skip(27L +  msgLength)//171+msgLength
+        this.input.skip(17L +  msgLength)//161+msgLength
+
+        this.input.mark(113)//161+msgLength
+
+        clientKey = this.input.readNBytes(112)//273+msgLength
+
+        this.input.reset()//161+msgLength
+
+        this.input.skip(10)//171+msgLength
 
         this._0828_rec_decr_key = this.input.readNBytes(16)//187+msgLength
 
-        this.input.skip(437L)//187+msgLength
+        this.input.skip(2)//189+msgLength
+
+        token88 = this.input.readNBytes(136)//325+msgLength
+
+        this.input.skip(299L)//624+msgLength
 
         //varString (nickLength bytes)
         val nickLength = this.input.readByte().toUByte().toInt()//625+msgLength
@@ -59,11 +71,18 @@ class ServerLoginResponseSucceedPacket(input: DataInputStream, val packetDataLen
 
         val dataIndex = packetDataLength - 31
 
+        this.input.skip((dataIndex - (625 + msgLength + nickLength)) + 0L)//-31
+
+        gender = this.input.readByte().toUByte().toInt()//-30
+
+        this.input.skip(9)//-27
+
+        age = this.input.readShort()//-25
+
+        /*
         age = HexToDec(取文本中间(data, 取文本长度(data) - 82, 5))
         gender = 取文本中间(data, 取文本长度(data) - 94, 2)
-        clientKey = 删全部空(取文本中间(data, 484 * 3 + msgLength + 1, 112 * 3 - 1))
-
-        token88 = 取文本中间(data, 568 + msgLength, 407)
+        */
     }
 }
 
