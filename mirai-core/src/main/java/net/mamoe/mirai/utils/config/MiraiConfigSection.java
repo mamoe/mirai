d27f6b8574d29c6bee49c47bb6eaccd6f20e8789
liujiahua123123 <liujiahua123123@126.com>
2019-08-21 11:19:04 +0800

    YAML supported

--- mirai-core/src/main/java/net/mamoe/mirai/utils/config/MiraiConfigSection.java
+++ mirai-core/src/main/java/net/mamoe/mirai/utils/config/MiraiConfigSection.java
@@ -27,4 +27,31 @@ public class MiraiConfigSection<T> extends ConcurrentSkipListMap<String, T> {
         return (D)(this.getOrDefault(key,defaultV));
     }
 
+    public Integer getInt(String key){
+        return Integer.valueOf(this.get(key).toString());
+    }
+
+    public Double getDouble(String key){
+        return Double.valueOf(this.get(key).toString());
+    }
+
+    public Float getFloat(String key){
+        return Float.valueOf(this.get(key).toString());
+    }
+
+    public String getString(String key){
+        return String.valueOf(this.get(key));
+    }
+
+    @SuppressWarnings("unchecked")
+    public <D extends T> MiraiConfigSection<D> getTypedSection(String key){
+        return (MiraiConfigSection<D>) this.getAs(key);
+    }
+
+    @SuppressWarnings("unchecked")
+    public MiraiConfigSection<Object> getSection(String key){
+        return (MiraiConfigSection<Object>) this.getAs(key);
+    }
+
+
 }
