05e590c93f9d7de5f26000030922134b9e7948a8
Him188 <Him188@mamoe.net>
2019-10-19 13:51:16 +0800

    Convert to expression fun

--- mirai-core/src/commonMain/kotlin/net.mamoe.mirai/network/protocol/tim/packet/ServerPacket.kt
+++ mirai-core/src/commonMain/kotlin/net.mamoe.mirai/network/protocol/tim/packet/ServerPacket.kt
@@ -34,45 +34,16 @@ fun <S : ServerPacket> S.applySequence(sequenceId: UShort): S {
     return this
 }
 
-fun ServerPacket.decryptBy(key: ByteArray): ByteReadPacket {
-    return ByteReadPacket(decryptAsByteArray(key))
-}
+fun ServerPacket.decryptBy(key: ByteArray): ByteReadPacket = ByteReadPacket(decryptAsByteArray(key))
+fun ServerPacket.decryptBy(key: IoBuffer): ByteReadPacket = ByteReadPacket(decryptAsByteArray(key))
+fun ServerPacket.decryptBy(keyHex: String): ByteReadPacket = this.decryptBy(keyHex.hexToBytes())
 
-fun ServerPacket.decryptBy(key: IoBuffer): ByteReadPacket {
-    return ByteReadPacket(decryptAsByteArray(key))
-}
+fun ServerPacket.decryptBy(key1: ByteArray, key2: ByteArray): ByteReadPacket = TEA.decrypt(this.decryptAsByteArray(key1), key2).toReadPacket()
+fun ServerPacket.decryptBy(key1: String, key2: ByteArray): ByteReadPacket = this.decryptBy(key1.hexToBytes(), key2)
+fun ServerPacket.decryptBy(key1: String, key2: IoBuffer): ByteReadPacket = this.decryptBy(key1.hexToBytes(), key2.readBytes())
+fun ServerPacket.decryptBy(key1: ByteArray, key2: String): ByteReadPacket = this.decryptBy(key1, key2.hexToBytes())
+fun ServerPacket.decryptBy(keyHex1: String, keyHex2: String): ByteReadPacket = this.decryptBy(keyHex1.hexToBytes(), keyHex2.hexToBytes())
 
-fun ServerPacket.decryptBy(keyHex: String): ByteReadPacket {
-    return this.decryptBy(keyHex.hexToBytes())
-}
-
-fun ServerPacket.decryptBy(key1: ByteArray, key2: ByteArray): ByteReadPacket {
-    return TEA.decrypt(this.decryptAsByteArray(key1), key2).toReadPacket()
-}
-
-
-fun ServerPacket.decryptBy(key1: String, key2: ByteArray): ByteReadPacket {
-    return this.decryptBy(key1.hexToBytes(), key2)
-}
-
-fun ServerPacket.decryptBy(key1: String, key2: IoBuffer): ByteReadPacket {
-    return this.decryptBy(key1.hexToBytes(), key2.readBytes())
-}
-
-
-fun ServerPacket.decryptBy(key1: ByteArray, key2: String): ByteReadPacket {
-    return this.decryptBy(key1, key2.hexToBytes())
-}
-
-
-fun ServerPacket.decryptBy(keyHex1: String, keyHex2: String): ByteReadPacket {
-    return this.decryptBy(keyHex1.hexToBytes(), keyHex2.hexToBytes())
-}
-
-fun ServerPacket.decryptAsByteArray(key: ByteArray): ByteArray {
-    return TEA.decrypt(input.readRemainingBytes().cutTail(1), key)
-}
-
+fun ServerPacket.decryptAsByteArray(key: ByteArray): ByteArray = TEA.decrypt(input.readRemainingBytes().cutTail(1), key)
 fun ServerPacket.decryptAsByteArray(keyHex: String): ByteArray = this.decryptAsByteArray(keyHex.hexToBytes())
-
 fun ServerPacket.decryptAsByteArray(buffer: IoBuffer): ByteArray = this.decryptAsByteArray(buffer.readBytes())
