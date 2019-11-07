1f521e96dfe9886b20ee9a8c4612775036b8bae8
Him188 <Him188@mamoe.net>
2019-11-07 15:08:19 +0800

    Platform adjustment

--- mirai-core/src/androidMain/kotlin/net/mamoe/mirai/utils/DefaultCaptchaSolverAndroid.kt
+++ mirai-core/src/androidMain/kotlin/net/mamoe/mirai/utils/DefaultCaptchaSolverAndroid.kt
@@ -1,7 +1,7 @@
 package net.mamoe.mirai.utils
 
 /**
- * 直接抛出异常. 需自行处理验证码
+ * 直接抛出异常. 需自行处理验证码, 在 [BotConfiguration.captchaSolver] 中调整
  */
 actual var DefaultCaptchaSolver: CaptchaSolver = {
     error("No CaptchaSolver found. BotConfiguration.captchaSolver should be assigned manually")
