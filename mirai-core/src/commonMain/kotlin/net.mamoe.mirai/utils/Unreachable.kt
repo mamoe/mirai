87ad509f70f83c560cef2ede2f804a735703d139
Him188 <Him188@mamoe.net>
2019-11-17 14:09:33 +0800

    Fix typo

--- mirai-core/src/commonMain/kotlin/net.mamoe.mirai/utils/Unreachable.kt
+++ mirai-core/src/commonMain/kotlin/net.mamoe.mirai/utils/Unreachable.kt
@@ -4,4 +4,4 @@ package net.mamoe.mirai.utils
  * 表示这里是不可到达的位置.
  */
 @Suppress("NOTHING_TO_INLINE")
-internal inline fun assertUnreachable(): Nothing = error("This clause should be reached")
+internal inline fun assertUnreachable(): Nothing = error("This clause should not be reached")
