bf8e65e98b0dd7e610e5e2783b1e2f98abe9abcf
Him188 <Him188@mamoe.net>
2019-11-25 17:23:12 +0800

    Disable GroupFileUploadEventFactory

--- mirai-core/src/commonMain/kotlin/net.mamoe.mirai/network/protocol/tim/packet/event/GroupFileUpload.kt
+++ mirai-core/src/commonMain/kotlin/net.mamoe.mirai/network/protocol/tim/packet/event/GroupFileUpload.kt
@@ -6,6 +6,7 @@ import kotlinx.io.core.ByteReadPacket
 import kotlinx.io.core.discardExact
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.network.protocol.tim.packet.PacketVersion
+import net.mamoe.mirai.utils.io.debugPrint
 import net.mamoe.mirai.utils.io.readString
 
 
@@ -14,6 +15,8 @@ data class GroupFileUploadPacket(inline val xmlMessage: String) : EventPacket
 @PacketVersion(date = "2019.7.1", timVersion = "2.3.2 (21173)")
 object GroupFileUploadEventFactory : KnownEventParserAndHandler<GroupFileUploadPacket>(0x002Du) {
     override suspend fun ByteReadPacket.parse(bot: Bot, identity: EventPacketIdentity): GroupFileUploadPacket {
+        this.debugPrint("GroupFileUploadPacket")
+        return GroupFileUploadPacket("")
         discardExact(60)
         val size = readShort().toInt()
         discardExact(3)
