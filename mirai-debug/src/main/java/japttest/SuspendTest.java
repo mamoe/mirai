1aaa43b1334710a39d910baf55b8ba0e3efdb947
Him188 <Him188@mamoe.net>
2019-12-06 19:30:56 +0800

    Comment bug lines

--- mirai-debug/src/main/java/japttest/SuspendTest.java
+++ mirai-debug/src/main/java/japttest/SuspendTest.java
@@ -1,16 +1,14 @@
 package japttest;
 
-import net.mamoe.mirai.network.protocol.tim.packet.login.LoginResult;
-import test.JaptTestKt;
-
 @SuppressWarnings("unused")
 public class SuspendTest {
 
     public static void main(String[] args) {
         // TODO: 2019/12/6 Kotlin or IDE bug here
-        boolean bool = JaptTestKt.getLoginResult() == LoginResult.YOU_CAN_WRITE_ANY_THING;
+        // Uncomment the following line.
+        // boolean bool = JaptTestKt.getLoginResult() == LoginResult.YOU_CAN_WRITE_ANY_THING;
 
-        System.out.println("Hello world finished");
+        System.out.println("Hello world");
 
     }
 }
