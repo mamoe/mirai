21e7eafe04b0d67663d0903dcaf2cc17441ef1da
Him188 <Him188@mamoe.net>
2019-12-07 00:00:57 +0800

    Add test

--- mirai-core/src/commonTest/kotlin/net.mamoe.mirai.utils.io/TypeConversionTest.kt
+++ mirai-core/src/commonTest/kotlin/net.mamoe.mirai.utils.io/TypeConversionTest.kt
@@ -1,10 +1,20 @@
 package net.mamoe.mirai.utils.io
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.asserter
+
 
 class TypeConversionTest {
 
-    //@Test
+    @ExperimentalUnsignedTypes
+    @Test
     fun testConversions() {
-
+        assertEquals("01", byteArrayOf(1).toUHexString())
+        assertEquals("7F", byteArrayOf(0x7F).toUHexString())
+        assertEquals("FF", ubyteArrayOf(0xffu).toUHexString())
+        assertEquals("7F", ubyteArrayOf(0x7fu).toUHexString())
+        assertEquals(byteArrayOf(0, 0, 0, 0x01).toUHexString(), 1.toByteArray().toUHexString())
+        assertEquals(ubyteArrayOf(0x7fu, 0xffu, 0xffu, 0xffu).toByteArray().toUHexString(), Int.MAX_VALUE.toByteArray().toUHexString())
     }
 }
