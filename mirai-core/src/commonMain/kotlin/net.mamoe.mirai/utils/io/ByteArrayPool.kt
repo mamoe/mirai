d5fc392ecf845fd658f1cb486d37c22232dd8538
Him188 <Him188@mamoe.net>
2019-12-13 09:02:31 +0800

    Make ByteArrayPool internal

--- mirai-core/src/commonMain/kotlin/net.mamoe.mirai/utils/io/ByteArrayPool.kt
+++ mirai-core/src/commonMain/kotlin/net.mamoe.mirai/utils/io/ByteArrayPool.kt
@@ -6,7 +6,7 @@ import kotlinx.io.pool.ObjectPool
 internal const val DEFAULT_BYTE_ARRAY_POOL_SIZE = 256
 internal const val DEFAULT_BYTE_ARRAY_SIZE = 4096
 
-val ByteArrayPool: ObjectPool<ByteArray> = ByteArrayPoolImpl
+internal val ByteArrayPool: ObjectPool<ByteArray> = ByteArrayPoolImpl
 
 private object ByteArrayPoolImpl : DefaultPool<ByteArray>(DEFAULT_BYTE_ARRAY_POOL_SIZE) {
     override fun produceInstance(): ByteArray = ByteArray(DEFAULT_BYTE_ARRAY_SIZE)
