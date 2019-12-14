537c21452d0824d619d8ffee30b700d8bdb742c9
Him188 <Him188@mamoe.net>
2019-12-14 22:06:44 +0800

    Add Exceptions.kt

---
+++ mirai-core/src/commonMain/kotlin/net.mamoe.mirai/utils/Exceptions.kt
@@ -0,0 +1,20 @@
+@file:Suppress("unused", "UNUSED_PARAMETER")
+
+package net.mamoe.mirai.utils
+
+/**
+ * 在获取 [Group] 对象等操作时可能出现的异常
+ */
+class GroupNotFoundException : Exception {
+    constructor()
+    constructor(message: String?)
+    constructor(message: String?, cause: Throwable?)
+    constructor(cause: Throwable?)
+}
+
+open class MiraiInternalException : Exception {
+    constructor()
+    constructor(message: String?)
+    constructor(message: String?, cause: Throwable?)
+    constructor(cause: Throwable?)
+}
