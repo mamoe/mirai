f77107757258955452469561667fb6ee1099e3fc
Him188 <Him188@mamoe.net>
2019-12-15 15:27:56 +0800

    Cleanup

--- mirai-core/src/jvmMain/kotlin/net/mamoe/mirai/MiraiEnvironmentJvm.kt
+++ mirai-core/src/jvmMain/kotlin/net/mamoe/mirai/MiraiEnvironmentJvm.kt
@@ -2,13 +2,6 @@
 
 package net.mamoe.mirai
 
-import kotlinx.io.core.buildPacket
-import kotlinx.io.core.readBytes
-import net.mamoe.mirai.contact.GroupInternalId
-import net.mamoe.mirai.contact.toId
-import net.mamoe.mirai.utils.io.toByteArray
-import net.mamoe.mirai.utils.io.toUHexString
-
 actual object MiraiEnvironment {
     @JvmStatic
     actual val platform: Platform
