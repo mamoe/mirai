70201af1c8a1a6206188eb0850977d9516c9a451
Him188 <Him188@mamoe.net>
2020-01-10 22:48:57 +0800

    Add contract

--- mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/utils/inline.kt
+++ mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/utils/inline.kt
@@ -1,7 +1,17 @@
 package net.mamoe.mirai.qqandroid.utils
 
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
 /**
  * Inline the block
  */
+@UseExperimental(ExperimentalContracts::class)
 @PublishedApi
-internal inline fun <R> inline(block: () -> R): R = block()
+internal inline fun <R> inline(block: () -> R): R {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+    return block()
+}
