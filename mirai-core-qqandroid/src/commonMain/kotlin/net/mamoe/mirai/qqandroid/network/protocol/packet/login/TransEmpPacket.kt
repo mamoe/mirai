b54f93f1ec9e0842fab829f8bf1e6bdafeb8b456
Him188 <Him188@mamoe.net>
2020-01-12 18:52:24 +0800

    Add TransEmpPacket.kt

---
+++ mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/network/protocol/packet/login/TransEmpPacket.kt
@@ -0,0 +1,40 @@
+package net.mamoe.mirai.qqandroid.network.protocol.packet.login
+
+import kotlinx.io.core.ByteReadPacket
+import net.mamoe.mirai.data.Packet
+import net.mamoe.mirai.qqandroid.QQAndroidBot
+import net.mamoe.mirai.qqandroid.network.QQAndroidClient
+import net.mamoe.mirai.qqandroid.network.protocol.packet.*
+import net.mamoe.mirai.qqandroid.network.protocol.packet.OutgoingPacket
+import net.mamoe.mirai.qqandroid.network.protocol.packet.PacketFactory
+import net.mamoe.mirai.qqandroid.network.protocol.packet.buildLoginOutgoingPacket
+import net.mamoe.mirai.qqandroid.network.protocol.packet.writeOicqRequestPacket
+import net.mamoe.mirai.utils.io.toReadPacket
+
+internal object TransEmpPacket : PacketFactory<TransEmpPacket.Response, LoginPacketDecrypter>(LoginPacketDecrypter) {
+
+    init {
+        _id = PacketId(0x0812, "wtlogin.trans_emp")
+    }
+
+    private const val appId = 16L
+    private const val subAppId = 537062845L
+
+
+    fun SubCommand1(
+        client: QQAndroidClient
+    ): OutgoingPacket = buildLoginOutgoingPacket(client, subAppId, ssoExtraData = byteArrayOf().toReadPacket()) {
+        writeOicqRequestPacket(client, EncryptMethodECDH135(client.ecdh), id){
+
+            // oicq.wlogin_sdk.request.trans_emp_1#packTransEmpBody
+        }
+    }
+
+    object Response : Packet
+
+
+    override suspend fun ByteReadPacket.decode(bot: QQAndroidBot): Response {
+        TODO("not implemented")
+    }
+
+}
