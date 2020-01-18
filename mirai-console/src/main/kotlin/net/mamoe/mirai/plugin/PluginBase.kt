3204f2043921584b10e0f70bbef18a41bedbe744
Him188 <Him188@mamoe.net>
2020-01-18 23:10:01 +0800

    Close stream after read

--- mirai-console/src/main/kotlin/net/mamoe/mirai/plugin/PluginBase.kt
+++ mirai-console/src/main/kotlin/net/mamoe/mirai/plugin/PluginBase.kt
@@ -126,7 +126,9 @@ object PluginManager {
                     logger.info("plugin.yml not found in jar " + jar.name + ", it will not be consider as a Plugin")
                 } else {
                     val description =
-                        PluginDescription.readFromContent(URL("jar:file:" + file.absoluteFile + "!/" + pluginYml.name).openConnection().inputStream.readBytes().encodeToString())
+                        PluginDescription.readFromContent(URL("jar:file:" + file.absoluteFile + "!/" + pluginYml.name).openConnection().inputStream.use {
+                            it.readBytes().encodeToString()
+                        })
                     println(description)
                     pluginsFound[description.name] = description
                     pluginsLocation[description.name] = file
