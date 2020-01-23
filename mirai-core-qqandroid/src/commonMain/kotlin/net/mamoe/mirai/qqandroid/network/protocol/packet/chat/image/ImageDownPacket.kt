4c085039526e53b54114292e3c41b4ec18898b82
jiahua.liu <n@mamoe.net>
2020-01-23 21:06:41 +0800

    Android Protocol - Rebuild Image Protocol

--- mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/network/protocol/packet/chat/image/ImageDownPacket.kt
+++ mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/network/protocol/packet/chat/image/ImageDownPacket.kt
@@ -1,9 +1,13 @@
 package net.mamoe.mirai.qqandroid.network.protocol.packet.chat.image
 
 import kotlinx.io.core.ByteReadPacket
+import kotlinx.serialization.protobuf.ProtoBuf
 import net.mamoe.mirai.data.Packet
 import net.mamoe.mirai.qqandroid.QQAndroidBot
+import net.mamoe.mirai.qqandroid.network.QQAndroidClient
+import net.mamoe.mirai.qqandroid.network.protocol.packet.OutgoingPacket
 import net.mamoe.mirai.qqandroid.network.protocol.packet.PacketFactory
+import net.mamoe.mirai.qqandroid.network.protocol.packet.buildOutgingPacket
 
 internal object ImageDownPacket : PacketFactory<ImageDownPacket.ImageDownPacketResponse>() {
 
@@ -11,6 +15,15 @@ internal object ImageDownPacket : PacketFactory<ImageDownPacket.ImageDownPacketR
         this._commandName = "LongConn.OffPicDown"
     }
 
+
+    operator fun invoke(client: QQAndroidClient, req: GetImgUrlReq): OutgoingPacket {
+        return buildOutgingPacket(client, this._commandName, this._commandName, client.wLoginSigInfo.d2Key) {
+            ProtoBuf.dump(
+                Cmd0x325Packet.serializer(),
+                Cmd0x325Packet.createByImageRequest(req)
+            )
+        }
+    }
 
     override suspend fun ByteReadPacket.decode(bot: QQAndroidBot): ImageDownPacketResponse {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
--- mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/network/protocol/packet/chat/image/ImageUpPacket.kt
+++ mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/network/protocol/packet/chat/image/ImageUpPacket.kt
@@ -1,9 +1,13 @@
 package net.mamoe.mirai.qqandroid.network.protocol.packet.chat.image
 
 import kotlinx.io.core.ByteReadPacket
+import kotlinx.serialization.protobuf.ProtoBuf
 import net.mamoe.mirai.data.Packet
 import net.mamoe.mirai.qqandroid.QQAndroidBot
+import net.mamoe.mirai.qqandroid.network.QQAndroidClient
+import net.mamoe.mirai.qqandroid.network.protocol.packet.OutgoingPacket
 import net.mamoe.mirai.qqandroid.network.protocol.packet.PacketFactory
+import net.mamoe.mirai.qqandroid.network.protocol.packet.buildOutgingPacket
 
 internal object ImageUpPacket : PacketFactory<ImageUpPacket.ImageUpPacketResponse>() {
 
@@ -11,6 +15,15 @@ internal object ImageUpPacket : PacketFactory<ImageUpPacket.ImageUpPacketRespons
         this._commandName = "LongConn.OffPicUp"
     }
 
+
+    operator fun invoke(client: QQAndroidClient, req: UploadImgReq): OutgoingPacket {
+        return buildOutgingPacket(client, this._commandName, this._commandName, client.wLoginSigInfo.d2Key) {
+            ProtoBuf.dump(
+                Cmd0x325Packet.serializer(),
+                Cmd0x325Packet.createByImageRequest(req)
+            )
+        }
+    }
 
     override suspend fun ByteReadPacket.decode(bot: QQAndroidBot): ImageUpPacketResponse {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -20,6 +33,5 @@ internal object ImageUpPacket : PacketFactory<ImageUpPacket.ImageUpPacketRespons
     sealed class ImageUpPacketResponse : Packet {
         object Success : ImageUpPacketResponse()
     }
-
 
 }
