3d8c60cbd4d0793dae20ee86e8c14b37508221ef
jiahua.liu <n@mamoe.net>
2020-02-03 17:11:36 +0800

    Troop management

--- mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/network/protocol/packet/PacketFactory.kt
+++ mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/network/protocol/packet/PacketFactory.kt
@@ -5,6 +5,7 @@ import kotlinx.io.pool.useInstance
 import net.mamoe.mirai.data.Packet
 import net.mamoe.mirai.event.Subscribable
 import net.mamoe.mirai.qqandroid.QQAndroidBot
+import net.mamoe.mirai.qqandroid.network.protocol.packet.chat.TroopManagement
 import net.mamoe.mirai.qqandroid.network.protocol.packet.chat.receive.MessageSvc
 import net.mamoe.mirai.qqandroid.network.protocol.packet.chat.receive.OnlinePush
 import net.mamoe.mirai.qqandroid.network.protocol.packet.list.FriendList
@@ -117,7 +118,10 @@ internal object KnownPacketFactories {
         MessageSvc.PbSendMsg,
         FriendList.GetFriendGroupList,
         FriendList.GetTroopListSimplify,
-        FriendList.GetTroopMemberList
+        FriendList.GetTroopMemberList,
+        TroopManagement.EditNametag,
+        TroopManagement.Mute,
+        TroopManagement.MuteAll
     )
 
     object IncomingFactories : List<IncomingPacketFactory<*>> by mutableListOf(
