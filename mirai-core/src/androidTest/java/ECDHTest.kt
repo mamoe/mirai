9fe26540b2ecc6edb7c302cf03c24464a8bd701e
Him188 <Him188@mamoe.net>
2020-02-06 14:56:13 +0800

    Remove ECDHTest.kt

--- mirai-core/src/androidTest/java/ECDHTest.kt
+++
@@ -1,8 +0,0 @@
-import net.mamoe.mirai.timpc.network.TIMProtocol
-import net.mamoe.mirai.utils.io.toUHexString
-
-
-fun main() {
-
-    println(EcdhCrypt().calShareKeyMd5ByPeerPublicKey(TIMProtocol.publicKey).toUHexString())
-}
