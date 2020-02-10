6504283d4dd98de8f1741aa78ccb3d4d6bfa02b2
Him188 <Him188@mamoe.net>
2020-02-10 16:44:41 +0800

    Remove unused annotations

--- mirai-core/src/commonMain/kotlin/net.mamoe.mirai/utils/Annotations.kt
+++ mirai-core/src/commonMain/kotlin/net.mamoe.mirai/utils/Annotations.kt
@@ -62,18 +62,3 @@ annotation class MiraiDebugAPI(
 @Retention(AnnotationRetention.BINARY)
 @MustBeDocumented
 annotation class SinceMirai(val version: String)
-
-/**
- * 包的最后一次修改时间, 和分析时使用的 TIM 版本
- */
-@MustBeDocumented
-@Target(FUNCTION, CLASS, PROPERTY)
-@Retention(AnnotationRetention.SOURCE)
-annotation class PacketVersion(val date: String, val timVersion: String)
-
-/**
- * 带有这个注解的 [Packet] 将不会被记录在 log 中.
- */
-@Target(CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class NoLog
