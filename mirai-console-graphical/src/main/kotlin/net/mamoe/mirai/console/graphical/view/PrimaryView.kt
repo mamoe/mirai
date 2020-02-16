71b1fb347ef437ae7440f30f3c45a9b8d25336e9
ryoii <ryoii@foxmail.com>
2020-02-16 18:27:31 +0800

    init console graphical

---
+++ mirai-console-graphical/src/main/kotlin/net/mamoe/mirai/console/graphical/view/PrimaryView.kt
@@ -0,0 +1,11 @@
+package net.mamoe.mirai.console.graphical.view
+
+import tornadofx.View
+import tornadofx.borderpane
+
+class PrimaryView : View() {
+
+    override val root = borderpane {
+
+    }
+}
