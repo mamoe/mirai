83611ac09f001de829620b764d170aa9b7168856
ryoii <ryoii@foxmail.com>
2020-02-19 02:07:18 +0800

    f**k javafx

---
+++ mirai-console-graphical/src/main/kotlin/net/mamoe/mirai/console/graphical/view/Decorator.kt
@@ -0,0 +1,9 @@
+package net.mamoe.mirai.console.graphical.view
+
+import com.jfoenix.controls.JFXDecorator
+import tornadofx.View
+
+class Decorator: View() {
+
+    override val root = JFXDecorator(primaryStage, find<PrimaryView>().root)
+}
