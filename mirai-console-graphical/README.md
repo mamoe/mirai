c6ae8e32c2efb865b6692a1785077606522b19cf
Him188 <Him188@mamoe.net>
2020-02-27 13:30:15 +0800

    Separate `mirai-console` series from main repository

--- mirai-console-graphical/README.md
+++
@@ -1,6 +0,0 @@
-### Mirai Console Graphical 
-支持windows/mac
-有正式UI界面实现的CONSOLE
-优点: 适合新手/完全不懂编程的/界面美丽
-缺点: 不能在linux服务器运行
-所使用插件系统与terminal版本一致 可以来回切换
--- mirai-console-graphical/src/main/kotlin/net/mamoe/mirai/console/graphical/view/Decorator.kt
+++
@@ -1,9 +0,0 @@
-package net.mamoe.mirai.console.graphical.view
-
-import com.jfoenix.controls.JFXDecorator
-import tornadofx.View
-
-class Decorator: View() {
-
-    override val root = JFXDecorator(primaryStage, find<PrimaryView>().root)
-}
