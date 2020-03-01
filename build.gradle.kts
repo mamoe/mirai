6496c76a4d13458b45af1421c5a8c4b791121ee5
Him188 <Him188@mamoe.net>
2020-03-01 13:00:34 +0800

    Use build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import java.lang.System.getProperty
+import java.util.*
+
+buildscript {
+    repositories {
+        mavenLocal()
+        maven { setUrl("https://mirrors.huaweicloud.com/repository/maven") }
+        jcenter()
+        mavenCentral()
+        google()
+        maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+        maven { setUrl("https://dl.bintray.com/kotlin/kotlin-dev") }
+    }
+
+    dependencies {
+        val kotlinVersion: String by project
+        val atomicFuVersion: String by project
+
+        classpath("com.android.tools.build:gradle:3.5.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.github.jengelman.gradle.plugins:shadow:5.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:$atomicFuVersion")
+    }
+}
+
+runCatching {
+    val keyProps = Properties().apply {
+        file("local.properties").takeIf { it.exists() }?.inputStream()?.use { load(it) }
+    }
+    if (keyProps.getProperty("sdk.dir", "").isNotEmpty()) {
+        project.ext.set("isAndroidSDKAvailable", true)
+    } else {
+        project.ext.set("isAndroidSDKAvailable", false)
+    }
+}
+
+allprojects {
+    group = "net.mamoe"
+    version = getProperty("mirai_version")
+
+    repositories {
+        mavenLocal()
+        maven { setUrl("https://mirrors.huaweicloud.com/repository/maven") }
+        jcenter()
+        mavenCentral()
+        google()
+        maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+        maven { setUrl("https://dl.bintray.com/kotlin/kotlin-dev") }
+    }
+}
