b5bd36fb8fbec49bdde0cefff8599083071aaa6d
Him188 <Him188@mamoe.net>
2020-03-03 20:04:46 +0800

    Avoid self-suppression

--- mirai-core/src/jvmMain/kotlin/net/mamoe/mirai/utils/addSuppressed.kt
+++ mirai-core/src/jvmMain/kotlin/net/mamoe/mirai/utils/addSuppressed.kt
@@ -5,6 +5,9 @@ private var isAddSuppressedSupported: Boolean = true
 @MiraiInternalAPI
 @Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 actual fun Throwable.addSuppressed(e: Throwable) {
+    if (this === e) {
+        return
+    }
     if (!isAddSuppressedSupported) {
         return
     }
