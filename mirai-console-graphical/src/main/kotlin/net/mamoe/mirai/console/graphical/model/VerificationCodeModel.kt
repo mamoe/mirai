5ece8e95f9fbe62d3ce8868197b553d531348275
ryoii <ryoii@foxmail.com>
2020-03-20 00:08:44 +0800

    Graphic `VerificationCode`

--- mirai-console-graphical/src/main/kotlin/net/mamoe/mirai/console/graphical/model/VerificationCodeModel.kt
+++ mirai-console-graphical/src/main/kotlin/net/mamoe/mirai/console/graphical/model/VerificationCodeModel.kt
@@ -1,17 +1,26 @@
 package net.mamoe.mirai.console.graphical.model
 
+import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.ItemViewModel
 import tornadofx.getValue
 import tornadofx.setValue
 
-class VerificationCode {
-    val codeProperty = SimpleStringProperty("")
-    var code: String by codeProperty
+class VerificationCode(data: ByteArray = ByteArray(0)) {
+    val codeProperty = SimpleStringProperty(null)
+    var code: String? by codeProperty
+
+    val dataProperty: SimpleObjectProperty<ByteArray> = SimpleObjectProperty()
+    val data: ByteArray by dataProperty
+
+    init {
+        dataProperty.set(data)
+    }
 }
 
 class VerificationCodeModel(code: VerificationCode) : ItemViewModel<VerificationCode>(code) {
     constructor() : this(VerificationCode())
 
     val code = bind(VerificationCode::codeProperty)
+    val data = bind(VerificationCode::dataProperty)
 }
--- mirai-console-graphical/src/main/kotlin/net/mamoe/mirai/console/graphical/view/VerificationCodeFragment.kt
+++ mirai-console-graphical/src/main/kotlin/net/mamoe/mirai/console/graphical/view/VerificationCodeFragment.kt
@@ -1,16 +1,38 @@
 package net.mamoe.mirai.console.graphical.view
 
+import javafx.scene.image.Image
+import net.mamoe.mirai.console.graphical.model.VerificationCodeModel
 import tornadofx.*
+import java.io.ByteArrayInputStream
 
 class VerificationCodeFragment : Fragment() {
 
+    companion object {
+        val MAGIC_KEY = String("CANCEL".toByteArray())
+    }
+
+    val code = find<VerificationCodeModel>()
+
     override val root = vbox {
-        //TODO: 显示验证码
+        // 显示验证码
+        imageview(Image(ByteArrayInputStream(code.data.value)))
 
         form {
             fieldset {
                 field("验证码") {
-                    textfield()
+                    textfield(code.code)
+                }
+            }
+
+            buttonbar {
+                button("提交").action {
+                    code.commit()
+                    this@VerificationCodeFragment.close()
+                }
+                button("取消").action {
+                    code.code.value = MAGIC_KEY
+                    code.commit()
+                    this@VerificationCodeFragment.close()
                 }
             }
         }
