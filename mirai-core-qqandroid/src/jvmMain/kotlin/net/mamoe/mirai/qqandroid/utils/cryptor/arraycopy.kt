8d7e984b1c4c66960e4bee22d94b0c40c9cea743
Him188 <Him188@mamoe.net>
2020-03-27 22:19:14 +0800

    Remove useless files

--- mirai-core-qqandroid/src/jvmMain/kotlin/net/mamoe/mirai/qqandroid/utils/cryptor/arraycopy.kt
+++
@@ -1,9 +0,0 @@
-package net.mamoe.mirai.qqandroid.utils.cryptor
-
-internal actual fun arraycopy(
-    src: ByteArray,
-    srcPos: Int,
-    dest: ByteArray,
-    destPos: Int,
-    length: Int
-) = System.arraycopy(src, srcPos, dest, destPos, length)
