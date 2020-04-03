985b0b4ba4abebbf9943d3353c53cb6f045b7fcc
Him188 <Him188@mamoe.net>
2020-04-03 08:44:18 +0800

    Make `FlashImage` constructable from `mirai-core`

--- mirai-core/src/commonMain/kotlin/net.mamoe.mirai/message/data/FlashImage.kt
+++ mirai-core/src/commonMain/kotlin/net.mamoe.mirai/message/data/FlashImage.kt
@@ -1,15 +1,56 @@
+@file:Suppress("NOTHING_TO_INLINE", "unused")
+
 package net.mamoe.mirai.message.data
 
+import net.mamoe.mirai.utils.SinceMirai
+import kotlin.jvm.JvmSynthetic
+
+/**
+ * 闪照
+ *
+ * @see Image.flash
+ */
+@SinceMirai("")
 sealed class FlashImage : MessageContent {
-    companion object Key : Message.Key<FlashImage>
+    companion object Key : Message.Key<FlashImage> {
+        operator fun invoke(image: Image): FlashImage {
+            return when (image) {
+                is GroupImage -> GroupFlashImage(image)
+                is FriendImage -> FriendFlashImage(image)
+                else -> throw IllegalArgumentException("不支持的图片类型(Please use GroupImage or FriendImage)")
+            }
+        }
+    }
 
-    abstract val image : Image
+    /**
+     * 闪照的图片, 不同于普通的图片.
+     */
+    abstract val image: Image
+
+    private var stringValue: String? = null
+        get() {
+            return field ?: kotlin.run {
+                field = "[mirai:flash:${image.imageId}]"
+                field
+            }
+        }
+
+    override fun toString(): String = stringValue!!
+    override val length: Int get() = stringValue!!.length
+    override fun get(index: Int) = stringValue!![index]
+    override fun subSequence(startIndex: Int, endIndex: Int) = stringValue!!.subSequence(startIndex, endIndex)
+    override fun compareTo(other: String) = other.compareTo(stringValue!!)
 }
 
-abstract class AbstractGroupFlashImage : FlashImage() {
-    abstract override val image: GroupImage
-}
+@JvmSynthetic
+inline fun Image.flash(): FlashImage = FlashImage(this)
 
-abstract class AbstractFriendFlashImage : FlashImage() {
-    abstract override val image: FriendImage
-}
+@JvmSynthetic
+inline fun GroupImage.flash(): GroupFlashImage = FlashImage(this) as GroupFlashImage
+
+@JvmSynthetic
+inline fun FriendImage.flash(): FriendFlashImage = FlashImage(this) as FriendFlashImage
+
+class GroupFlashImage internal constructor(override val image: GroupImage) : FlashImage()
+
+class FriendFlashImage internal constructor(override val image: FriendImage) : FlashImage()
