c95efe98a3a17ec1fd51917d465c86c6f8d7068d
Him188 <Him188@mamoe.net>
2020-04-05 23:09:12 +0800

    Fix `addSuppressedMirai` on JVM

--- mirai-core-qqandroid/src/jvmMain/kotlin/net/mamoe/mirai/qqandroid/utils/addSuppressedMirai.kt
+++ mirai-core-qqandroid/src/jvmMain/kotlin/net/mamoe/mirai/qqandroid/utils/addSuppressedMirai.kt
@@ -2,4 +2,10 @@ package net.mamoe.mirai.qqandroid.utils
 
 @PublishedApi
 internal actual fun Throwable.addSuppressedMirai(e: Throwable) {
+    if (e === this) {
+        return
+    }
+    kotlin.runCatching {
+        this.addSuppressed(e)
+    }
 }
