515c285ac6d5b3ebe12052a524f5cd453876b7d8
Karlatemp <karlatemp@vip.qq.com>
2020-04-29 13:06:55 +0800

    Fix unused

--- mirai-core/src/jvmTest/kotlin/net/mamoe/mirai/utils/StepUtil.kt
+++ mirai-core/src/jvmTest/kotlin/net/mamoe/mirai/utils/StepUtil.kt
@@ -6,7 +6,7 @@ class StepUtil {
     val step = atomic(0)
     fun step(step: Int, message: String = "Wrong step") {
         if (step != this.step.getAndIncrement()) {
-            throw IllegalStateException("Step exception")
+            throw IllegalStateException(message)
         }
         println("Invoking step $step")
     }
