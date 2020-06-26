b9342be3829af622a574f62f6461fe6ce4fedcf2
Him188 <Him188@mamoe.net>
2020-06-27 01:28:32 +0800

    Rename SimpleCommand to RawCommand

--- backend/mirai-console/src/main/kotlin/net/mamoe/mirai/console/command/RawCommand.kt
+++ backend/mirai-console/src/main/kotlin/net/mamoe/mirai/console/command/RawCommand.kt
@@ -1,10 +1,10 @@
 package net.mamoe.mirai.console.command
 
-abstract class SimpleCommand(
+abstract class RawCommand(
     override val owner: CommandOwner,
     override vararg val names: String,
-    override val usage: String,
-    override val description: String,
+    override val usage: String = "<no usages given>",
+    override val description: String = "<no descriptions given>",
     override val permission: CommandPermission = CommandPermission.Default,
     override val prefixOptional: Boolean = false
 ) : Command {
