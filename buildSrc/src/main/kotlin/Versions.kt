7c5a62d18e25b3d79f9a128d67e5896a5eecfa95
Him188 <Him188@mamoe.net>
2020-06-29 22:55:23 +0800

    pure 0.1-dev-1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,7 @@ object Versions {
     const val console = "1.0-dev-2"
     const val consoleGraphical = "0.0.7"
     const val consoleTerminal = "0.1.0"
-    const val consolePure = "0.1.0"
+    const val consolePure = "0.1-dev-1"
 
     const val kotlin = "1.3.72"
     const val coroutines = "1.3.7"
