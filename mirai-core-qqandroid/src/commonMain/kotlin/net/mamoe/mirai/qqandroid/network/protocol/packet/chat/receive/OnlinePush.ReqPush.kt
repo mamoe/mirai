2e7a3269165902b67b5fbede9d0a9b3868f294a8
Him188 <Him188@mamoe.net>
2020-07-02 01:27:48 +0800

    Suppress DEPRECATION for internal usage

--- mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/network/protocol/packet/chat/receive/OnlinePush.ReqPush.kt
+++ mirai-core-qqandroid/src/commonMain/kotlin/net/mamoe/mirai/qqandroid/network/protocol/packet/chat/receive/OnlinePush.ReqPush.kt
@@ -257,6 +257,7 @@ private object Transformers732 : Map<Int, Lambda732> by mapOf(
                     }
                 }
 
+                @Suppress("DEPRECATION")
                 if (group.settings.isConfessTalkEnabled == new) {
                     return@lambda732 emptySequence()
                 }
