1ca6c7be6c35908bc31d1309e0c2544fd26c63b5
Him188 <Him188@mamoe.net>
2020-07-11 20:13:50 +0800

    Use same version from console for console-pure

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,10 +9,10 @@
 
 object Versions {
     const val core = "1.0.3"
-    const val console = "1.0-dev-2"
+    const val console = "1.0-dev-3"
     const val consoleGraphical = "0.0.7"
     const val consoleTerminal = "0.1.0"
-    const val consolePure = "0.1-dev-1"
+    const val consolePure = console
 
     const val kotlin = "1.4-M3"
     const val coroutines = "1.3.7"
