241d40bd646da969203851fec8f04399119ca0f8
Him188 <Him188@mamoe.net>
2020-08-01 22:32:24 +0800

    Add Versions.kt

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/mamoe/mirai/blob/master/LICENSE
+ */
+
+/*
+ * Copyright 2020 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/mamoe/mirai/blob/master/LICENSE
+ */
+
+object Versions {
+    const val core = "1.1.3"
+    const val console = "1.0-M1"
+    const val consoleGraphical = "0.0.7"
+    const val consoleTerminal = "0.1.0"
+    const val consolePure = console
+
+    const val kotlinCompiler = "1.4.0-rc" // for public explict API
+    const val kotlinStdlib = "1.4.0-rc" // for not overriding dependant's stdlib dependency
+
+    const val coroutines = "1.3.8-1.4.0-rc"
+    const val collectionsImmutable = "0.3.2"
+    const val serialization = "1.0-M1-1.4.0-rc"
+    const val ktor = "1.3.2-1.4.0-rc"
+
+    const val androidGradle = "3.6.2"
+
+    const val bintray = "1.8.5"
+}
