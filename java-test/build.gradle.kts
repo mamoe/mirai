236cf2540d80b2c3677d0447ef8885ff18821714
Him188 <Him188@mamoe.net>
2020-08-16 23:54:37 +0800

    Fix build

--- java-test/build.gradle.kts
+++ java-test/build.gradle.kts
@@ -19,22 +19,4 @@ dependencies {
     implementation(project(":mirai-serialization"))
 
     testImplementation(group = "junit", name = "junit", version = "4.12")
-
-    implementation(kotlin("stdlib", null))
-    implementation(kotlin("serialization", null))
-    implementation(kotlin("reflect", null))
-
-
-    implementation(kotlinx("serialization-runtime-common", Versions.Kotlin.serialization))
-    implementation(kotlinx("serialization-protobuf-common", Versions.Kotlin.serialization))
-    implementation(kotlinx("io", Versions.Kotlin.io))
-    implementation(kotlinx("coroutines-io", Versions.Kotlin.coroutinesIo))
-    implementation(kotlinx("coroutines-core-common", Versions.Kotlin.coroutines))
-
-    implementation("org.jetbrains.kotlinx:atomicfu-common:${Versions.Kotlin.atomicFU}")
-
-    implementation(ktor("client-cio", Versions.Kotlin.ktor))
-    implementation(ktor("client-core", Versions.Kotlin.ktor))
-    implementation(ktor("network", Versions.Kotlin.ktor))
-
 }
