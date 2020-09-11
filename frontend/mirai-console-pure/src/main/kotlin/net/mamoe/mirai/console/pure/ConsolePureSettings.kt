12b942a27bf7fa1d9dc55dbac9f6d6d94163a8bb
Karlatemp <karlatemp@vip.qq.com>
2020-09-11 19:28:12 +0800

    Fix Copyright

--- frontend/mirai-console-pure/src/main/kotlin/net/mamoe/mirai/console/pure/ConsolePureSettings.kt
+++ frontend/mirai-console-pure/src/main/kotlin/net/mamoe/mirai/console/pure/ConsolePureSettings.kt
@@ -1,14 +1,11 @@
 /*
- * Copyright (c) 2018-2020 Karlatemp. All rights reserved.
- * @author Karlatemp <karlatemp@vip.qq.com> <https://github.com/Karlatemp>
+ * Copyright 2019-2020 Mamoe Technologies and contributors.
  *
- * LuckPerms-Mirai/mirai-console.mirai-console-pure.main/ConsolePureSettings.kt
- *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found via the following link.
  *
- * https://github.com/Karlatemp/LuckPerms-Mirai/blob/master/LICENSE
+ * https://github.com/mamoe/mirai/blob/master/LICENSE
  */
-
 /*
  * @author Karlatemp <karlatemp@vip.qq.com> <https://github.com/Karlatemp>
  */
