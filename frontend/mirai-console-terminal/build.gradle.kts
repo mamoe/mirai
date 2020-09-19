100bc1040473f379559604c7865a2fd9bdce8fde
Him188 <Him188@mamoe.net>
2020-09-20 00:28:57 +0800

    Fix shadow

--- frontend/mirai-console-terminal/build.gradle.kts
+++ frontend/mirai-console-terminal/build.gradle.kts
@@ -36,34 +36,12 @@ dependencies {
     implementation("org.jline:jline:3.15.0")
     implementation("org.fusesource.jansi:jansi:1.18")
 
-    compileAndRuntime(project(":mirai-console"))
-    compileAndRuntime("net.mamoe:mirai-core:${Versions.core}")
-    compileAndRuntime(kotlin("stdlib", Versions.kotlinStdlib)) // embedded by core
+    compileAndTestRuntime(project(":mirai-console"))
+    compileAndTestRuntime("net.mamoe:mirai-core:${Versions.core}")
+    compileAndTestRuntime(kotlin("stdlib-jdk8", Versions.kotlinStdlib)) // embedded by core
 
-    runtimeOnly("net.mamoe:mirai-core-qqandroid:${Versions.core}")
     testApi("net.mamoe:mirai-core-qqandroid:${Versions.core}")
     testApi(project(":mirai-console"))
-
-
-//    val autoService = "1.0-rc7"
-//    kapt("com.google.auto.service", "auto-service", autoService)
-//    compileOnly("com.google.auto.service", "auto-service-annotations", autoService)
-//    testCompileOnly("com.google.auto.service", "auto-service-annotations", autoService)
-}
-
-ext.apply {
-    // 傻逼 compileAndRuntime 没 exclude 掉
-    // 傻逼 gradle 第二次配置 task 会覆盖掉第一次的配置
-    val x: com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar.() -> Unit = {
-        dependencyFilter.include {
-            when ("${it.moduleGroup}:${it.moduleName}") {
-                "org.jline:jline" -> true
-                "org.fusesource.jansi:jansi" -> true
-                else -> false
-            }
-        }
-    }
-    this.set("shadowJar", x)
 }
 
 version = Versions.consoleTerminal
