2c5f9ebcf44bab59d694d470420bc3b20ac718cc
Him188 <Him188@mamoe.net>
2020-10-28 13:35:15 +0800

    Update copyright

--- backend/mirai-console/src/main/kotlin/net/mamoe/mirai/console/MiraiConsole.kt
+++ backend/mirai-console/src/main/kotlin/net/mamoe/mirai/console/MiraiConsole.kt
@@ -2,7 +2,7 @@
  * Copyright 2019-2020 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found via the following link.
+ * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found through the following link.
  *
  * https://github.com/mamoe/mirai/blob/master/LICENSE
  */
@@ -17,7 +17,6 @@ import kotlinx.coroutines.Job
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.console.MiraiConsole.INSTANCE
 import net.mamoe.mirai.console.MiraiConsoleImplementation.Companion.start
-import net.mamoe.mirai.console.command.BuiltInCommands
 import net.mamoe.mirai.console.extensions.BotConfigurationAlterer
 import net.mamoe.mirai.console.internal.MiraiConsoleImplementationBridge
 import net.mamoe.mirai.console.internal.extension.GlobalComponentStorage
--- frontend/mirai-console-terminal/src/main/kotlin/net/mamoe/mirai/console/terminal/ConsoleTerminalSettings.kt
+++ frontend/mirai-console-terminal/src/main/kotlin/net/mamoe/mirai/console/terminal/ConsoleTerminalSettings.kt
@@ -2,10 +2,9 @@
  * Copyright 2019-2020 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found via the following link.
+ * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found through the following link.
  *
  * https://github.com/mamoe/mirai/blob/master/LICENSE
- *
  */
 /*
  * @author Karlatemp <karlatemp@vip.qq.com> <https://github.com/Karlatemp>
@@ -27,7 +26,7 @@ package net.mamoe.mirai.console.terminal
 annotation class ConsoleTerminalExperimentalApi
 
 @ConsoleTerminalExperimentalApi
-public object ConsoleTerminalSettings {
+object ConsoleTerminalSettings {
     @JvmField
     var setupAnsi: Boolean = System.getProperty("os.name")
         .toLowerCase()
