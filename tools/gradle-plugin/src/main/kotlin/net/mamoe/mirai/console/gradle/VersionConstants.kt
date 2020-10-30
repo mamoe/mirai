8f18842503851f6da71529ab0d4465eb32f08575
Him188 <Him188@mamoe.net>
2020-10-30 10:01:57 +0800

    Update version constants

--- tools/gradle-plugin/src/main/kotlin/net/mamoe/mirai/console/gradle/VersionConstants.kt
+++ tools/gradle-plugin/src/main/kotlin/net/mamoe/mirai/console/gradle/VersionConstants.kt
@@ -10,6 +10,6 @@
 package net.mamoe.mirai.console.gradle
 
 internal object VersionConstants {
-    const val CONSOLE_VERSION = "1.0-RC-dev-33" // value is written here automatically during build
+    const val CONSOLE_VERSION = "1.0-RC" // value is written here automatically during build
     const val CORE_VERSION = "1.3.2" // value is written here automatically during build
 }
