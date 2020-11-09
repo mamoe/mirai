9066c9a1174de97701081047597203d7d8ed43f7
Him188 <Him188@mamoe.net>
2020-11-09 09:16:14 +0800

    1.0-RC2-dev-1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,17 +1,17 @@
 /*
-* Copyright 2019-2020 Mamoe Technologies and contributors.
-*
-* 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
-* Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found through the following link.
-*
-* https://github.com/mamoe/mirai/blob/master/LICENSE
-*/
+ * Copyright 2019-2020 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found through the following link.
+ *
+ * https://github.com/mamoe/mirai/blob/master/LICENSE
+ */
 
 @file:Suppress("MemberVisibilityCanBePrivate")
 
 object Versions {
     const val core = "1.3.3"
-    const val console = "1.0-RC-1"
+    const val console = "1.0-RC2-dev-1"
     const val consoleGraphical = "0.0.7"
     const val consoleTerminal = console
 
