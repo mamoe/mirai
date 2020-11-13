9a25ac78fa5611c5cfde96300d451e11053421c0
Him188 <Him188@mamoe.net>
2020-11-13 18:24:40 +0800

    1.0-RC2-dev-6

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -11,7 +11,7 @@
 
 object Versions {
     const val core = "1.3.3"
-    const val console = "1.0-RC2-dev-4"
+    const val console = "1.0-RC2-dev-6"
     const val consoleGraphical = "0.0.7"
     const val consoleTerminal = console
 
