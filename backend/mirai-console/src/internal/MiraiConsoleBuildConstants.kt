525b759d9e1523e68424898f32fd7bd7d4f0315b
Him188 <Him188@mamoe.net>
2020-11-29 00:27:03 +0800

    1.1.0-dev-30

--- backend/mirai-console/src/internal/MiraiConsoleBuildConstants.kt
+++ backend/mirai-console/src/internal/MiraiConsoleBuildConstants.kt
@@ -14,8 +14,8 @@ import java.time.Instant
 
 internal object MiraiConsoleBuildConstants { // auto-filled on build (task :mirai-console:fillBuildConstants)
     @JvmStatic
-    val buildDate: Instant = Instant.ofEpochSecond(1606580129)
-    const val versionConst: String = "1.1.0-dev-29"
+    val buildDate: Instant = Instant.ofEpochSecond(1606580812)
+    const val versionConst: String = "1.1.0-dev-30"
 
     @JvmStatic
     val version: SemVersion = SemVersion(versionConst)
--- tools/gradle-plugin/src/VersionConstants.kt
+++ tools/gradle-plugin/src/VersionConstants.kt
@@ -10,6 +10,6 @@
 package net.mamoe.mirai.console.gradle
 
 internal object VersionConstants {
-    const val CONSOLE_VERSION = "1.1.0-dev-29" // value is written here automatically during build
+    const val CONSOLE_VERSION = "1.1.0-dev-30" // value is written here automatically during build
     const val CORE_VERSION = "1.3.3" // value is written here automatically during build
 }
