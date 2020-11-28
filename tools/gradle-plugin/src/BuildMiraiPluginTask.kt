b36ddf29e29e0f732a5553fc3152ab963e859c68
Him188 <Him188@mamoe.net>
2020-11-29 00:26:13 +0800

    Fix gradle tasks

--- tools/gradle-plugin/src/BuildMiraiPluginTask.kt
+++ tools/gradle-plugin/src/BuildMiraiPluginTask.kt
@@ -2,17 +2,24 @@ package net.mamoe.mirai.console.gradle
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.OutputFile
 import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
 import java.io.File
 
 @CacheableTask
 public open class BuildMiraiPluginTask : ShadowJar() {
-    internal var targetField: KotlinTarget? = null
+    @Internal
+    public var targetField: KotlinTarget? = null
 
-    public val target: KotlinTarget get() = targetField!!
+    @get:Internal
+    public val target: KotlinTarget
+        get() = targetField!!
 
     /**
      * ShadowJar 打包结果
      */
-    public val output: File get() = outputs.files.singleFile
+    @get:OutputFile
+    public val output: File
+        get() = outputs.files.singleFile
 }
