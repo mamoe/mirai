02345597acd8e4e13f2f9df192eff8beeb623012
Him188 <Him188@mamoe.net>
2020-12-18 16:38:25 +0800

    2.0-M1-dev-1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -13,7 +13,7 @@ import org.gradle.api.attributes.Attribute
 
 object Versions {
     const val core = "2.0-M1-dev-3"
-    const val console = "1.1.0"
+    const val console = "2.0-M1-dev-1"
     const val consoleGraphical = "0.0.7"
     const val consoleTerminal = console
 
