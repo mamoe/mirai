471e70205e36a05572d0a2c85dcb66eeb955aaa2
Karlatemp <karlatemp@vip.qq.com>
2020-12-26 16:43:10 +0800

    Covert SimpleListenerHostTest to kotlin

---
+++ mirai-core-api/src/jvmTest/kotlin/event/SimpleListenerHostTestJava.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019-2020 Mamoe Technologies and contributors.
+ *
+ *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ *  https://github.com/mamoe/mirai/blob/master/LICENSE
+ */
+
+package net.mamoe.mirai.event
+
+import kotlinx.coroutines.CoroutineScope
+import net.mamoe.mirai.JavaFriendlyAPI
+import net.mamoe.mirai.utils.EventListenerLikeJava
+import org.junit.jupiter.api.Test
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.coroutines.EmptyCoroutineContext
+
+@JavaFriendlyAPI
+@EventListenerLikeJava
+class SimpleListenerHostTestJava {
+    @Test
+    fun testJavaSimpleListenerHostWork() {
+        val called = AtomicBoolean()
+        val host: SimpleListenerHost = object : SimpleListenerHost() {
+            @EventHandler
+            @EventListenerLikeJava
+            fun testListen(
+                event: AbstractEvent?
+            ) {
+                println(event)
+                called.set(true)
+            }
+        }
+        val scope = CoroutineScope(EmptyCoroutineContext)
+        scope.registerEvents(host)
+        object : AbstractEvent() {}.__broadcastJava()
+        if (!called.get()) {
+            throw AssertionError("JavaTest: SimpleListenerHost Failed.")
+        }
+    }
+}
