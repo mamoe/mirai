3511649bd0ddcf84255a6eeabf26305c14f9648a
Karlatemp <karlatemp@vip.qq.com>
2020-12-27 18:09:52 +0800

    2.0-M2-dev-10

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,8 +12,8 @@
 import org.gradle.api.attributes.Attribute
 
 object Versions {
-    const val core = "2.0-M2-dev-4"
-    const val console = "2.0-M2-dev-4"
+    const val core = "2.0-M2-dev-10"
+    const val console = "2.0-M2-dev-10"
     const val consoleGraphical = "0.0.7"
     const val consoleTerminal = console
 
