5e24e47c843d24ace222cc622539c4d93371fe50
Him188 <Him188@mamoe.net>
2021-01-10 20:32:45 +0800

    2.0-RC-dev-8

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,7 @@
 import org.gradle.api.attributes.Attribute
 
 object Versions {
-    const val project = "2.0-RC-dev-7"
+    const val project = "2.0-RC-dev-8"
 
     const val kotlinCompiler = "1.4.21"
     const val kotlinStdlib = "1.4.21"
