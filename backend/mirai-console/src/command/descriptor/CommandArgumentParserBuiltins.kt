8fde96c50a79f1891e5dac6552497093b6ae6b76
Him188 <Him188@mamoe.net>
2021-01-29 12:23:24 +0800

    Fix doc 'since' in EnumValueArgumentParser

--- backend/mirai-console/src/command/descriptor/CommandArgumentParserBuiltins.kt
+++ backend/mirai-console/src/command/descriptor/CommandArgumentParserBuiltins.kt
@@ -1,10 +1,10 @@
 /*
- * Copyright 2019-2020 Mamoe Technologies and contributors.
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
  *
- * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found through the following link.
+ *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
  *
- * https://github.com/mamoe/mirai/blob/master/LICENSE
+ *  https://github.com/mamoe/mirai/blob/master/LICENSE
  */
 
 @file:Suppress("EXPOSED_SUPER_CLASS")
@@ -379,7 +379,7 @@ public object RawContentValueArgumentParser : CommandValueArgumentParser<Message
  * - `STD_IN`, `STD_OUT`, `STD_ERR`  (忽视大小写)
  * - `stdIn`,  `stdOut`,  `stdErr`   (不忽视大小写)
  *
- * @since 2.1.0
+ * @since 2.2
  */
 public class EnumValueArgumentParser<T : Enum<T>>(
     private val type: Class<T>,
