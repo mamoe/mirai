3a5e2fe1748d6a9a694c5601c252a9e64ecf1f77
Karlatemp <karlatemp@vip.qq.com>
2021-02-03 22:11:14 +0800

    Mark `@JvmWildcard` on Command.overloads for fix java cannot extends XXXCommand

--- backend/mirai-console/src/command/RawCommand.kt
+++ backend/mirai-console/src/command/RawCommand.kt
@@ -1,10 +1,10 @@
 /*
- * Copyright 2019-2020 Mamoe Technologies and contributors.
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
  *
- * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found through the following link.
+ *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
  *
- * https://github.com/mamoe/mirai/blob/master/LICENSE
+ *  https://github.com/mamoe/mirai/blob/master/LICENSE
  */
 
 @file:Suppress("unused")
@@ -60,7 +60,7 @@ public abstract class RawCommand(
     public override val permission: Permission by lazy { findOrCreateCommandPermission(parentPermission) }
 
     @ExperimentalCommandDescriptors
-    override val overloads: List<CommandSignature> = listOf(
+    override val overloads: List<@JvmWildcard CommandSignature> = listOf(
         CommandSignatureImpl(
             receiverParameter = CommandReceiverParameter(false, typeOf0<CommandSender>()),
             valueParameters = listOf(AbstractCommandValueParameter.UserDefinedType.createRequired<Array<out Message>>("args", true))
