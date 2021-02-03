b7c46e27e05819e0071ddaa5697e4422324409e1
Him188 <Him188@mamoe.net>
2021-02-03 13:15:23 +0800

    2.3.0

--- backend/mirai-console/src/internal/MiraiConsoleBuildConstants.kt
+++ backend/mirai-console/src/internal/MiraiConsoleBuildConstants.kt
@@ -14,8 +14,8 @@ import java.time.Instant
 
 internal object MiraiConsoleBuildConstants { // auto-filled on build (task :mirai-console:fillBuildConstants)
     @JvmStatic
-    val buildDate: Instant = Instant.ofEpochSecond(1612105641)
-    const val versionConst: String = "2.2.2"
+    val buildDate: Instant = Instant.ofEpochSecond(1612329270)
+    const val versionConst: String = "2.3.0"
 
     @JvmStatic
     val version: SemVersion = SemVersion(versionConst)
