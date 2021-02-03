41bbef98ad5312150c014913ec4ad7bf20062348
Him188 <Him188@mamoe.net>
2021-02-03 15:49:27 +0800

    2.3.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,7 @@
 import org.gradle.api.attributes.Attribute
 
 object Versions {
-    const val project = "2.3.0"
+    const val project = "2.3.1"
 
     const val core = project
     const val console = project
