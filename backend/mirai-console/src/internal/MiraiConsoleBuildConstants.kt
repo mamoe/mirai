626b8d600b53bece733d9c378546b802e3ba39c4
Him188 <Him188@mamoe.net>
2021-02-07 14:35:20 +0800

    2.4-M1

--- backend/mirai-console/src/internal/MiraiConsoleBuildConstants.kt
+++ backend/mirai-console/src/internal/MiraiConsoleBuildConstants.kt
@@ -14,8 +14,8 @@ import java.time.Instant
 
 internal object MiraiConsoleBuildConstants { // auto-filled on build (task :mirai-console:fillBuildConstants)
     @JvmStatic
-    val buildDate: Instant = Instant.ofEpochSecond(1612539167)
-    const val versionConst: String = "2.4.0-dev-1"
+    val buildDate: Instant = Instant.ofEpochSecond(1612679428)
+    const val versionConst: String = "2.4-M1"
 
     @JvmStatic
     val version: SemVersion = SemVersion(versionConst)
--- tools/intellij-plugin/run/projects/test-project/build.gradle.kts
+++ tools/intellij-plugin/run/projects/test-project/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.4.20"
     kotlin("plugin.serialization") version "1.4.20"
-    id("net.mamoe.mirai-console") version "2.3.2"
+    id("net.mamoe.mirai-console") version "2.4-M1"
     java
 }
 
