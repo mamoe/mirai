5bf1a9755fb3bf440a5427cbf2bf8ae05b1beec6
Karlatemp <karlatemp@vip.qq.com>
2021-02-16 22:44:01 +0800

    Improve PsiMethod.hasSignature; fix #294

--- tools/intellij-plugin/run/projects/test-project/src/main/java/test/TestJavaPlugin.java
+++ tools/intellij-plugin/run/projects/test-project/src/main/java/test/TestJavaPlugin.java
@@ -3,6 +3,7 @@ package test;
 import net.mamoe.mirai.console.command.Command;
 import net.mamoe.mirai.console.command.CommandOwner;
 import net.mamoe.mirai.console.command.descriptor.CommandSignatureFromKFunction;
+import net.mamoe.mirai.console.command.java.JCompositeCommand;
 import net.mamoe.mirai.console.command.java.JSimpleCommand;
 import net.mamoe.mirai.console.permission.Permission;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
@@ -26,5 +27,18 @@ class TestCommand extends JSimpleCommand {
     @Handler
     public void test(String s) {
 
+    }
+}
+
+class TestCommand2 extends JCompositeCommand {
+    public TestCommand2(@NotNull CommandOwner owner, @NotNull String primaryName, @NotNull String[] secondaryNames, @NotNull Permission parentPermission) {
+        super(owner, primaryName, secondaryNames, parentPermission);
+    }
+
+    @SubCommand("test")
+    public void test() {}
+
+    @SubCommand({})
+    public void subCmd() {
     }
 }
