2eaa13a7c21815f96eae61f8559691105ea0cb48
Karlatemp <karlatemp@vip.qq.com>
2021-02-18 01:15:48 +0800

    Release 2.5.0-dev-2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,7 @@
 import org.gradle.api.attributes.Attribute
 
 object Versions {
-    const val project = "2.5.0-dev-1"
+    const val project = "2.5.0-dev-2"
 
     const val core = project
     const val console = project
