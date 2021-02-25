f010b1150ccb4a45ef1a0d13e015eb6669b3fada
Him188 <Him188@mamoe.net>
2021-02-25 09:41:05 +0800

    Fix platform declarations in mirai-core-utils

---
+++ mirai-core-utils/src/androidMain/kotlin/Actuals.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
+ *
+ *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ *  https://github.com/mamoe/mirai/blob/master/LICENSE
+ */
+
+@file:JvmMultifileClass
+@file:Suppress("NOTHING_TO_INLINE")
+
+package net.mamoe.mirai.utils
+
+import android.util.Base64
+
+
+public actual fun ByteArray.encodeToBase64(): String {
+    return Base64.encodeToString(this, Base64.DEFAULT)
+}
