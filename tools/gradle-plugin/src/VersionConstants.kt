2e042f6ccd9898d3b0a2b003719008f88b2cb435
Him188 <Him188@mamoe.net>
2021-02-27 14:45:25 +0800

    Release version 2.5-M1

--- tools/gradle-plugin/src/VersionConstants.kt
+++ tools/gradle-plugin/src/VersionConstants.kt
@@ -10,6 +10,6 @@
 package net.mamoe.mirai.console.gradle
 
 internal object VersionConstants {
-    const val CONSOLE_VERSION = "2.4.0" // value is written here automatically during build
-    const val CORE_VERSION = "2.4.0" // value is written here automatically during build
+    const val CONSOLE_VERSION = "2.5-M1" // value is written here automatically during build
+    const val CORE_VERSION = "2.5-M1" // value is written here automatically during build
 }
