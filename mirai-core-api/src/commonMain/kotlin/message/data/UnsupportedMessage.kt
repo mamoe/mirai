62498c87e1511c9eaf4aa83519f5fbc8c0349736
Him188 <Him188@mamoe.net>
2021-03-26 19:25:54 +0800

    `UnsupportedMessage.contentToString`: use `struct.contentHashCode()`

--- mirai-core-api/src/commonMain/kotlin/message/data/UnsupportedMessage.kt
+++ mirai-core-api/src/commonMain/kotlin/message/data/UnsupportedMessage.kt
@@ -33,7 +33,7 @@ import net.mamoe.mirai.utils.toUHexString
 @SerialName(UnsupportedMessage.SERIAL_NAME)
 @Serializable(UnsupportedMessage.Serializer::class)
 public interface UnsupportedMessage : MessageContent {
-    override fun contentToString(): String = "[不支持的消息#${hashCode()}]"
+    override fun contentToString(): String = "[不支持的消息#${struct.contentHashCode()}]" // to produce 'stable' and reliable text
 
     /**
      * 原生消息数据
