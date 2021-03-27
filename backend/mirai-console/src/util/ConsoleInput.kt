b6417ac336af8c08462a0c5f9c2fb0dd273ef287
Him188 <Him188@mamoe.net>
2021-03-27 22:40:42 +0800

    Fix doc on ConsoleInput

--- backend/mirai-console/src/util/ConsoleInput.kt
+++ backend/mirai-console/src/util/ConsoleInput.kt
@@ -1,10 +1,10 @@
 /*
- * Copyright 2019-2020 Mamoe Technologies and contributors.
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
  *
- * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found through the following link.
+ *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
  *
- * https://github.com/mamoe/mirai/blob/master/LICENSE
+ *  https://github.com/mamoe/mirai/blob/master/LICENSE
  */
 
 @file:Suppress("INAPPLICABLE_JVM_NAME", "unused")
@@ -18,7 +18,7 @@ import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.internal.util.ConsoleInputImpl
 
 /**
- * Console 输入. 由于 console 接管了 [标准输入][System. in], [readLine] 等操作需要在这里进行.
+ * Console 输入. 由于 console 接管了 [标准输入][System.in], [readLine] 等操作需要在这里进行.
  */
 public interface ConsoleInput {
     /**
