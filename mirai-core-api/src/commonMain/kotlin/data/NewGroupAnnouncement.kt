a9f9faf76f8ce27d86de3d92a9052306189e8354
younger <highmagic@outlook.com>
2021-04-02 13:37:56 +0800

    Change to interface and API dump

--- mirai-core-api/src/commonMain/kotlin/data/NewGroupAnnouncement.kt
+++ mirai-core-api/src/commonMain/kotlin/data/NewGroupAnnouncement.kt
@@ -12,70 +12,77 @@ package net.mamoe.mirai.data
 import net.mamoe.mirai.utils.MiraiExperimentalApi
 import java.time.Instant
 
+/**
+ * 群公告.
+ */
 @MiraiExperimentalApi
-public class Announcement(
-    /**
-     *  公告的fid，每个公告仅有一条fid，类似于主键
-     */
-    public val fid: String,
+public interface Announcement {
 
     /**
      *  公告发送者的QQ号
      */
-    public val senderId: Long,
+    public val senderId: Long
 
     /**
      *  公告的标题
      */
-    public val title: String,
+    public val title: String
 
     /**
      *  公告的内容
      */
-    public val msg: String,
+    public val msg: String
 
     /**
      *  是否发送给新成员
      */
-    public val sendToNewMember: Boolean = false,
-
-    /**
-     *  公告发出的时间，为EpochSecond(自 1970-01-01T00：00：00Z 的秒数)
-     *  只读属性
-     *
-     * @see Instant.ofEpochSecond
-     */
-    public val publishTime: Long = 0,
+    public val sendToNewMember: Boolean
 
     /**
      *  是否置顶，可以有多个置顶公告
      */
-    public val isPinned: Boolean = false,
+    public val isPinned: Boolean
 
     /**
      *  是否显示能够引导群成员修改昵称窗口
      */
-    public val isShowEditCard: Boolean = false,
+    public val isShowEditCard: Boolean
 
     /**
      *  是否使用弹窗
      */
-    public val isTip: Boolean = false,
+    public val isTip: Boolean
 
     /**
      *  是否需要群成员确认
      */
-    public val needConfirm: Boolean = false,
+    public val needConfirm: Boolean
+}
+
+@MiraiExperimentalApi
+public interface ReadAnnouncement : Announcement {
+
+    /**
+     *  公告的fid，每个公告仅有一条fid，类似于主键
+     */
+    public val fid: String
 
     /**
      *  所有人都已阅读, 如果[Announcement.needConfirm]为true则为所有人都已确认,
-     *  只读属性
      */
-    public val isAllRead: Boolean = false,
+    public val isAllRead: Boolean
 
     /**
      *  已经阅读的成员数量，如果[Announcement.needConfirm]为true则为已经确认的成员数量
-     *  只读属性
      */
-    public val readMemberNumber: Int = 0
-)
+    public val readMemberNumber: Int
+
+
+    /**
+     *  公告发出的时间，为EpochSecond(自 1970-01-01T00：00：00Z 的秒数)
+     *
+     * @see Instant.ofEpochSecond
+     */
+    public val publishTime: Long
+}
+
