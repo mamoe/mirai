21e8852e0fb04d66f313881dff7e10278037735e
AdoptOSS <adoptoss@outlook.com>
2021-04-05 22:35:38 +0800

    buildscript: fix task not found errors when android sdk not available (#1161)
    * buildscript: fix task not found errors when android sdk not available
    
    * fix: this shadowed by run
    
    * cleanup: code style
    
    * improve: avoid breaking index map

--- mirai-core/build.gradle.kts
+++ mirai-core/build.gradle.kts
@@ -114,18 +114,20 @@ kotlin {
     }
 }
 
-tasks.register("checkAndroidApiLevel") {
-    doFirst {
-        analyzes.AndroidApiLevelCheck.check(
-            buildDir.resolve("classes/kotlin/android/main"),
-            project.property("mirai.android.target.api.level")!!.toString().toInt(),
-            project
-        )
+if (isAndroidSDKAvailable) {
+    tasks.register("checkAndroidApiLevel") {
+        doFirst {
+            analyzes.AndroidApiLevelCheck.check(
+                buildDir.resolve("classes/kotlin/android/main"),
+                project.property("mirai.android.target.api.level")!!.toString().toInt(),
+                project
+            )
+        }
+        group = "verification"
+        this.mustRunAfter("androidMainClasses")
     }
-    group = "verification"
-    this.mustRunAfter("androidMainClasses")
+    tasks.getByName("androidTest").dependsOn("checkAndroidApiLevel")
 }
-tasks.getByName("androidTest").dependsOn("checkAndroidApiLevel")
 
 fun org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler.implementation1(dependencyNotation: String) =
     implementation(dependencyNotation) {
