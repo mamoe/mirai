d81fd5276f26b543cba27a78c23512bc7fe1c3c8
Him188 <Him188@mamoe.net>
2021-04-10 18:59:47 +0800

    Release version 2.6-RC

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,7 @@
 import org.gradle.api.attributes.Attribute
 
 object Versions {
-    const val project = "2.6-M2"
+    const val project = "2.6-RC"
 
     const val core = project
     const val console = project
