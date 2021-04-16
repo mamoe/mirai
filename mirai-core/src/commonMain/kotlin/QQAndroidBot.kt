997ad1eb63df0460a9cbc5bddd9331364185e30b
Him188 <Him188@mamoe.net>
2021-04-16 21:17:50 +0800

    Rearrange QQAndroidBot

--- mirai-core/src/commonMain/kotlin/QQAndroidBot.kt
+++ mirai-core/src/commonMain/kotlin/QQAndroidBot.kt
@@ -12,13 +12,11 @@ package net.mamoe.mirai.internal
 
 import kotlinx.coroutines.sync.Mutex
 import net.mamoe.mirai.Bot
-import net.mamoe.mirai.LowLevelApi
 import net.mamoe.mirai.Mirai
-import net.mamoe.mirai.contact.*
+import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.contact.OtherClientInfo
 import net.mamoe.mirai.internal.contact.OtherClientImpl
 import net.mamoe.mirai.internal.contact.checkIsGroupImpl
-import net.mamoe.mirai.internal.contact.info.FriendInfoImpl
-import net.mamoe.mirai.internal.contact.info.StrangerInfoImpl
 import net.mamoe.mirai.internal.contact.uin
 import net.mamoe.mirai.internal.network.*
 import net.mamoe.mirai.internal.network.handler.*
@@ -56,58 +54,72 @@ internal class QQAndroidBot constructor(
     internal val account: BotAccount,
     configuration: BotConfiguration
 ) : AbstractBot(configuration, account.id), SsoContext {
-    val bdhSyncer: BdhSessionSyncer = BdhSessionSyncer(this)
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Account secrets cache
-    ///////////////////////////////////////////////////////////////////////////
-
-    // We cannot extract these logics until we rewrite the network framework.
-
-    private val cacheDir: File by lazy {
-        configuration.workingDir.resolve(bot.configuration.cacheDir).apply { mkdirs() }
-    }
-    internal val accountSecretsFile: File by lazy {
-        cacheDir.resolve("account.secrets")
-    }
-
-    private fun saveSecrets(secrets: AccountSecretsImpl) {
-        if (secrets.wLoginSigInfoField == null) return
-
-        accountSecretsFile.writeBytes(
-            TEA.encrypt(
-                secrets.toByteArray(AccountSecretsImpl.serializer()),
-                account.passwordMd5
-            )
-        )
-
-        network.context.logger.info { "Saved account secrets to local cache for fast login." }
-    }
-
-    init {
-        if (configuration.loginCacheEnabled) {
-            eventChannel.parentScope(this).subscribeAlways<WtLogin.Login.LoginPacketResponse> { event ->
-                if (event is WtLogin.Login.LoginPacketResponse.Success) {
-                    if (client.wLoginSigInfoInitialized) {
-                        saveSecrets(AccountSecretsImpl(client))
-                    }
-                }
-            }
-        }
-    }
-
-    /////////////////////////// accounts secrets end
-
     override lateinit var client: QQAndroidClient
-
-
     override val bot: QQAndroidBot get() = this
 
+    val bdhSyncer: BdhSessionSyncer = BdhSessionSyncer(this)
     internal var firstLoginSucceed: Boolean = false
 
-    inline val json get() = configuration.json
+    ///////////////////////////////////////////////////////////////////////////
+    // network
+    ///////////////////////////////////////////////////////////////////////////
 
-    override val friends: ContactList<Friend> = ContactList()
+    // TODO: 2021/4/14         bdhSyncer.loadFromCache()  when login
+
+    override suspend fun sendLogout() {
+        network.sendWithoutExpect(StatSvc.Register.offline(client))
+    }
+
+    override fun createNetworkHandler(coroutineContext: CoroutineContext): NetworkHandler {
+        val context = NetworkHandlerContextImpl(this, this)
+        return SelectorNetworkHandler(
+            context,
+            FactoryKeepAliveNetworkHandlerSelector(NettyNetworkHandlerFactory, serverListNew, context)
+        ) // We can move the factory to configuration but this is not necessary for now.
+    }
+
+
+    suspend inline fun <E : Packet> OutgoingPacketWithRespType<E>.sendAndExpect(
+        timeoutMillis: Long = 5000,
+        retry: Int = 2
+    ): E = network.run { sendAndExpect(timeoutMillis, retry) }
+
+    suspend inline fun <E : Packet> OutgoingPacket.sendAndExpect(timeoutMillis: Long = 5000, retry: Int = 2): E =
+        network.run { sendAndExpect(timeoutMillis, retry) }
+
+    /**
+     * 获取 获取群公告 所需的 bkn 参数
+     * */
+    val bkn: Int
+        get() = client.wLoginSigInfo.sKey.data
+            .fold(5381) { acc: Int, b: Byte -> acc + acc.shl(5) + b.toInt() }
+            .and(Int.MAX_VALUE)
+
+    ///////////////////////////////////////////////////////////////////////////
+    // contacts
+    ///////////////////////////////////////////////////////////////////////////
+
+    override lateinit var nick: String
+
+    @JvmField
+    val groupListModifyLock = Mutex()
+
+    // internally visible only
+    fun getGroupByUin(uin: Long): Group {
+        return getGroupByUinOrNull(uin)
+            ?: throw NoSuchElementException("Group ${Mirai.calculateGroupCodeByGroupUin(uin)} not found")
+    }
+
+    fun getGroupByUinOrNull(uin: Long): Group? {
+        return groups.firstOrNull { it.checkIsGroupImpl(); it.uin == uin }
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // contact cache
+    ///////////////////////////////////////////////////////////////////////////
+
+    inline val json get() = configuration.json
 
     val friendListCache: FriendListCache? by lazy {
         if (!configuration.contactListCache.friendListCacheEnabled) return@lazy null
@@ -146,58 +158,44 @@ internal class QQAndroidBot constructor(
         }
     }
 
-    override lateinit var nick: String
+    ///////////////////////////////////////////////////////////////////////////
+    // Account secrets cache
+    ///////////////////////////////////////////////////////////////////////////
 
-    override val asFriend: Friend by lazy {
-        @OptIn(LowLevelApi::class)
-        Mirai.newFriend(this, FriendInfoImpl(uin, nick, ""))
+    // We cannot extract these logics until we rewrite the network framework.
+
+    private val cacheDir: File by lazy {
+        configuration.workingDir.resolve(bot.configuration.cacheDir).apply { mkdirs() }
+    }
+    private val accountSecretsFile: File by lazy {
+        cacheDir.resolve("account.secrets")
     }
 
-    override val groups: ContactList<Group> = ContactList()
+    private fun saveSecrets(secrets: AccountSecretsImpl) {
+        if (secrets.wLoginSigInfoField == null) return
 
-    // TODO: 2021/4/14         bdhSyncer.loadFromCache()  when login
+        accountSecretsFile.writeBytes(
+            TEA.encrypt(
+                secrets.toByteArray(AccountSecretsImpl.serializer()),
+                account.passwordMd5
+            )
+        )
 
-    override suspend fun sendLogout() {
-        network.sendWithoutExpect(StatSvc.Register.offline(client))
+        network.context.logger.info { "Saved account secrets to local cache for fast login." }
     }
 
-    override fun createNetworkHandler(coroutineContext: CoroutineContext): NetworkHandler {
-        val context = NetworkHandlerContextImpl(this, this)
-        return SelectorNetworkHandler(
-            context,
-            FactoryKeepAliveNetworkHandlerSelector(NettyNetworkHandlerFactory, serverListNew, context)
-        ) // We can move the factory to configuration but this is not necessary for now.
+    init {
+        if (configuration.loginCacheEnabled) {
+            eventChannel.parentScope(this).subscribeAlways<WtLogin.Login.LoginPacketResponse> { event ->
+                if (event is WtLogin.Login.LoginPacketResponse.Success) {
+                    if (client.wLoginSigInfoInitialized) {
+                        saveSecrets(AccountSecretsImpl(client))
+                    }
+                }
+            }
+        }
     }
 
-    @JvmField
-    val groupListModifyLock = Mutex()
+    /////////////////////////// accounts secrets end
 
-    // internally visible only
-    fun getGroupByUin(uin: Long): Group {
-        return getGroupByUinOrNull(uin)
-            ?: throw NoSuchElementException("Group ${Mirai.calculateGroupCodeByGroupUin(uin)} not found")
-    }
-
-    fun getGroupByUinOrNull(uin: Long): Group? {
-        return groups.firstOrNull { it.checkIsGroupImpl(); it.uin == uin }
-    }
-
-
-    suspend inline fun <E : Packet> OutgoingPacketWithRespType<E>.sendAndExpect(
-        timeoutMillis: Long = 5000,
-        retry: Int = 2
-    ): E = network.run { sendAndExpect(timeoutMillis, retry) }
-
-    suspend inline fun <E : Packet> OutgoingPacket.sendAndExpect(timeoutMillis: Long = 5000, retry: Int = 2): E =
-        network.run { sendAndExpect(timeoutMillis, retry) }
-
-    /**
-     * 获取 获取群公告 所需的 bkn 参数
-     * */
-    val bkn: Int
-        get() = client.wLoginSigInfo.sKey.data
-            .fold(5381) { acc: Int, b: Byte -> acc + acc.shl(5) + b.toInt() }
-            .and(Int.MAX_VALUE)
-    override val asStranger: Stranger by lazy { Mirai.newStranger(bot, StrangerInfoImpl(bot.id, bot.nick)) }
-    override val strangers: ContactList<Stranger> = ContactList()
 }
