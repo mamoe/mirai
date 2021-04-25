2dc96999f1c3bfb22a5c4f3f521e8a7d814decd4
Him188 <Him188@mamoe.net>
2021-04-25 11:39:46 +0800

    2.7-M1-dev-2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,7 @@
 import org.gradle.api.attributes.Attribute
 
 object Versions {
-    const val project = "2.7-M1-dev-1"
+    const val project = "2.7-M1-dev-2"
 
     const val core = project
     const val console = project
