0e699c4b98e7e57cbc1da17a39f046fe773df6fb
Him188 <Him188@mamoe.net>
2021-04-26 21:30:51 +0800

    Support network selector retry

--- mirai-core/src/commonMain/kotlin/network/handler/selector/AbstractKeepAliveNetworkHandlerSelector.kt
+++ mirai-core/src/commonMain/kotlin/network/handler/selector/AbstractKeepAliveNetworkHandlerSelector.kt
@@ -13,6 +13,8 @@ import kotlinx.atomicfu.atomic
 import kotlinx.coroutines.yield
 import net.mamoe.mirai.internal.network.handler.NetworkHandler
 import net.mamoe.mirai.internal.network.handler.NetworkHandlerFactory
+import net.mamoe.mirai.utils.systemProp
+import net.mamoe.mirai.utils.toLongUnsigned
 import org.jetbrains.annotations.TestOnly
 
 /**
@@ -26,7 +28,14 @@ import org.jetbrains.annotations.TestOnly
  * and new connections are created only when calling [getResumedInstance] if the old connection was dead.
  */
 // may be replaced with a better name.
-internal abstract class AbstractKeepAliveNetworkHandlerSelector<H : NetworkHandler> : NetworkHandlerSelector<H> {
+internal abstract class AbstractKeepAliveNetworkHandlerSelector<H : NetworkHandler>(
+    private val maxAttempts: Int = DEFAULT_MAX_ATTEMPTS
+) : NetworkHandlerSelector<H> {
+
+    init {
+        require(maxAttempts >= 1) { "maxAttempts must >= 1" }
+    }
+
     private val current = atomic<H?>(null)
 
     @TestOnly
@@ -38,19 +47,23 @@ internal abstract class AbstractKeepAliveNetworkHandlerSelector<H : NetworkHandl
 
     final override fun getResumedInstance(): H? = current.value
 
-    final override tailrec suspend fun awaitResumeInstance(): H { // TODO: 2021/4/18 max 5 retry
+    final override suspend fun awaitResumeInstance(): H = awaitResumeInstanceImpl(0)
+
+    private tailrec suspend fun awaitResumeInstanceImpl(attempted: Int): H {
+        if (attempted >= maxAttempts) error("Failed to resume instance. Maximum attempts reached.")
         yield()
         val current = getResumedInstance()
         return if (current != null) {
-            when (current.state) {
+            when (val thisState = current.state) {
                 NetworkHandler.State.CLOSED -> {
                     this.current.compareAndSet(current, null) // invalidate the instance and try again.
-                    awaitResumeInstance() // will create new instance.
+                    awaitResumeInstanceImpl(attempted + 1) // will create new instance.
                 }
                 NetworkHandler.State.CONNECTING,
                 NetworkHandler.State.INITIALIZED -> {
-                    current.resumeConnection()
-                    return awaitResumeInstance()
+                    current.resumeConnection() // once finished, it should has been LOADING or OK
+                    check(current.state != thisState) { "State is still $thisState after successful resumeConnection." }
+                    return awaitResumeInstanceImpl(attempted) // does not count for an attempt.
                 }
                 NetworkHandler.State.LOADING -> {
                     return current
@@ -61,8 +74,17 @@ internal abstract class AbstractKeepAliveNetworkHandlerSelector<H : NetworkHandl
                 }
             }
         } else {
-            this.current.compareAndSet(current, createInstance())
-            awaitResumeInstance()
+            synchronized(this) { // avoid concurrent `createInstance()`
+                if (getResumedInstance() == null) this.current.compareAndSet(null, createInstance())
+            }
+            awaitResumeInstanceImpl(attempted) // directly retry, does not count for attempts.
         }
+    }
+
+    companion object {
+        @JvmField
+        var DEFAULT_MAX_ATTEMPTS =
+            systemProp("mirai.network.handler.selector.max.attempts", 3)
+                .coerceIn(1..Int.MAX_VALUE.toLongUnsigned()).toInt()
     }
 }
