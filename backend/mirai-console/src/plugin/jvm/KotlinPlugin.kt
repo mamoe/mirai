3321cdeac3b4d662c39bd7af7558f0b06ab8a568
Him188 <Him188@mamoe.net>
2021-04-30 23:19:34 +0800

    Fix ambiguous doc on KotlinPlugin

--- backend/mirai-console/src/plugin/jvm/KotlinPlugin.kt
+++ backend/mirai-console/src/plugin/jvm/KotlinPlugin.kt
@@ -1,10 +1,10 @@
 /*
- * Copyright 2019-2020 Mamoe Technologies and contributors.
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
  *
- * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE version 3 license that can be found through the following link.
+ *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
  *
- * https://github.com/mamoe/mirai/blob/master/LICENSE
+ *  https://github.com/mamoe/mirai/blob/master/LICENSE
  */
 
 @file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE", "EXPOSED_SUPER_CLASS", "RedundantVisibilityModifier")
@@ -16,8 +16,6 @@ import kotlin.coroutines.EmptyCoroutineContext
 
 /**
  * Kotlin 插件的父类.
- *
- * 必须通过 "plugin.yml" 指定主类并由 [JvmPluginLoader] 加载.
  */
 public abstract class KotlinPlugin @JvmOverloads constructor(
     public final override val description: JvmPluginDescription,
