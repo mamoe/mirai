2e1ec03daaf6a1e508bc1bc5baeb72796f10c270
Him188 <Him188@mamoe.net>
2021-05-09 01:26:28 +0800

    Deprecate ForceOfflineException

--- mirai-core-api/src/commonMain/kotlin/network/ForceOfflineException.kt
+++ mirai-core-api/src/commonMain/kotlin/network/ForceOfflineException.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 Mamoe Technologies and contributors.
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
  *
  *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -12,11 +12,11 @@ package net.mamoe.mirai.network
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Job
 import net.mamoe.mirai.Bot
-import kotlin.jvm.JvmOverloads
 
 /**
  * 当 [Bot] 被迫下线时抛出, 作为 [Job.cancel] 的 `cause`
  */
+@Deprecated("Not used anymore since 2.7", level = DeprecationLevel.WARNING)
 public class ForceOfflineException
 @JvmOverloads constructor(
     public override val message: String? = null,
