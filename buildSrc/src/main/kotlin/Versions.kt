a54aa62f235f85d147d47c67c6a94378c7e69bc7
Him188 <Him188@mamoe.net>
2021-05-30 23:20:39 +0800

    Release version 2.6.5

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,7 @@
 import org.gradle.api.attributes.Attribute
 
 object Versions {
-    const val project = "2.6.4"
+    const val project = "2.6.5"
 
     const val core = project
     const val console = project
