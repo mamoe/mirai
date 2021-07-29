6a63f1416febde02dcf14f11960e9bc6b09b2e8f
Karlatemp <karlatemp@vip.qq.com>
2021-07-29 12:46:38 +0800

    Dont add `mavenCentral()`; fix #389

--- tools/gradle-plugin/src/main/kotlin/MiraiConsoleGradlePlugin.kt
+++ tools/gradle-plugin/src/main/kotlin/MiraiConsoleGradlePlugin.kt
@@ -154,7 +154,6 @@ public class MiraiConsoleGradlePlugin : Plugin<Project> {
         // plugins.apply("org.gradle.maven")
         plugins.apply(ShadowPlugin::class.java)
         plugins.apply(BintrayPlugin::class.java)
-        repositories.mavenCentral()
 
         afterEvaluate {
             configureCompileTarget()
