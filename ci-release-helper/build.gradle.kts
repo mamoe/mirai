3aed3b0516fc17a2e5189a6b45495f4e5f8a36ca
Him188 <Him188@mamoe.net>
2021-08-25 16:35:30 +0800

    Create tag on releasing snapshots

--- ci-release-helper/build.gradle.kts
+++ ci-release-helper/build.gradle.kts
@@ -6,8 +6,13 @@
  *
  *  https://github.com/mamoe/mirai/blob/master/LICENSE
  */
+import com.google.gson.Gson
+import io.ktor.client.*
+import io.ktor.client.request.*
 import keys.SecretKeys
+import kotlinx.coroutines.runBlocking
 import org.gradle.kotlin.dsl.support.useToRun
+import java.io.ByteArrayOutputStream
 import java.net.URL
 
 plugins {
@@ -28,32 +33,71 @@ tasks.register("updateSnapshotVersion") {
     dependsOn(gradle.includedBuild("snapshots-publishing").task(":check"))
 
     doLast {
-        var baseUrl = System.getenv("SNAPSHOTS_PUBLISHING_URL") ?: "https://repo.mirai.mamoe.net/snapshots/"
-        if (!baseUrl.endsWith('/')) {
-            baseUrl += "/"
-        }
-        baseUrl += "net/mamoe/mirai-core-utils/maven-metadata.xml"
-
-        val content = URL(baseUrl).openConnection().getInputStream().useToRun {
-            readBytes().decodeToString()
-        }
-
-        val branch = System.getenv("CURRENT_BRANCH_NAME")
-        logger.info("Current branch name is '$branch'")
-
-        val currentVersion = getLatestMiraiVersionForBranch(content, branch)
-
-        logger.info("Current newest version for this branch is '$currentVersion'")
-
-        val nextVersion = currentVersion.nextSnapshotVersion(branch).toString()
-
-        logger.info("Next snapshot version will be '$nextVersion'")
-
         rootProject.file("buildSrc/src/main/kotlin/Versions.kt").run {
             var text = readText()
             check(text.indexOf("project = \"${project.version}\"") != -1) { "Cannot find \"project = \\\"${project.version}\\\"\"" }
             text = text.replace("project = \"${project.version}\"", "project = \"${nextVersion}\"")
             writeText(text)
+        }
+    }
+}
+
+val nextVersion by lazy { getNextVersion() }
+
+fun getNextVersion(): String {
+    var baseUrl = System.getenv("SNAPSHOTS_PUBLISHING_URL") ?: "https://repo.mirai.mamoe.net/snapshots/"
+    if (!baseUrl.endsWith('/')) {
+        baseUrl += "/"
+    }
+    baseUrl += "net/mamoe/mirai-core-utils/maven-metadata.xml"
+
+    val content = URL(baseUrl).openConnection().getInputStream().useToRun {
+        readBytes().decodeToString()
+    }
+
+    val branch = System.getenv("CURRENT_BRANCH_NAME")
+    logger.info("Current branch name is '$branch'")
+
+    val currentVersion = getLatestMiraiVersionForBranch(content, branch)
+
+    logger.info("Current newest version for this branch is '$currentVersion'")
+
+    val nextVersion = currentVersion.nextSnapshotVersion(branch).toString()
+
+    logger.info("Next snapshot version will be '$nextVersion'")
+
+    return nextVersion
+}
+
+tasks.register("createTagOnGitHub") {
+    group = "mirai"
+    dependsOn(gradle.includedBuild("snapshots-publishing").task(":check"))
+
+    doLast {
+        val token = System.getenv("GITHUB_OAUTH_TOKEN")
+        require(!token.isNullOrBlank()) { "" }
+
+        val out = ByteArrayOutputStream()
+        exec {
+            commandLine("git rev-parse HEAD")
+            standardOutput = out
+            workingDir = rootProject.projectDir
+        }
+        val sha = out.toString()
+        logger.info("Current sha is $sha")
+
+        runBlocking {
+            val resp = HttpClient().post<String>("https://api.github.com/repos/mamoe/mirai/git/refs") {
+                header("Authorization", "token $token")
+                header("Accept", "application/vnd.github.v3+json")
+                body = Gson().toJson(
+                    mapOf(
+                        "ref" to "refs/tags/$nextVersion",
+                        "sha" to sha,
+                    )
+                )
+            }
+            logger.info(resp)
         }
     }
 }
