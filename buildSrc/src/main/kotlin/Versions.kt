c0d85f3e9d7c0ce9ed1773d741af2faed73095de
Him188 <Him188@mamoe.net>
2021-08-31 11:15:24 +0800

    Set version to 2.8.0-M1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,7 @@
 import org.gradle.api.attributes.Attribute
 
 object Versions {
-    const val project = "2.8-M1"
+    const val project = "2.8.0-M1"
 
     const val core = project
     const val console = project
