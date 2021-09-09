4ab905c7f8eb0df044ce2fe2a162fbf52a1584c0
Karlatemp <karlatemp@vip.qq.com>
2021-09-09 18:00:20 +0800

    2.7.1-dev-1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,7 @@
 import org.gradle.api.attributes.Attribute
 
 object Versions {
-    const val project = "2.7.1-alpha"
+    const val project = "2.7.1-dev-1"
 
     const val core = project
     const val console = project
