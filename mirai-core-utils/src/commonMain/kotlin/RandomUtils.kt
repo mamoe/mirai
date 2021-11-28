d5d0b358066e0eb386110ee24dbea6db4a391828
Him188 <Him188@mamoe.net>
2021-11-28 17:56:11 +0000

    Add `DeviceInfo.random(Random)` and implement `equals` and `hashCode` for `DeviceInfo`

--- mirai-core-utils/src/commonMain/kotlin/RandomUtils.kt
+++ mirai-core-utils/src/commonMain/kotlin/RandomUtils.kt
@@ -22,7 +22,8 @@ import kotlin.random.nextInt
 /**
  * 生成长度为 [length], 元素为随机 `0..255` 的 [ByteArray]
  */
-public fun getRandomByteArray(length: Int): ByteArray = ByteArray(length) { Random.nextInt(0..255).toByte() }
+public fun getRandomByteArray(length: Int, random: Random = Random): ByteArray =
+    ByteArray(length) { random.nextInt(0..255).toByte() }
 
 /**
  * 随机生成一个正整数
@@ -32,8 +33,8 @@ public fun getRandomUnsignedInt(): Int = Random.nextInt().absoluteValue
 /**
  * 随机生成长度为 [length] 的 [String].
  */
-public fun getRandomString(length: Int): String =
-    getRandomString(length, *defaultRanges)
+public fun getRandomString(length: Int, random: Random = Random): String =
+    getRandomString(length, *defaultRanges, random = random)
 
 private val defaultRanges: Array<CharRange> = arrayOf('a'..'z', 'A'..'Z', '0'..'9')
 private val intCharRanges: Array<CharRange> = arrayOf('0'..'9')
@@ -41,15 +42,15 @@ private val intCharRanges: Array<CharRange> = arrayOf('0'..'9')
 /**
  * 根据所给 [charRange] 随机生成长度为 [length] 的 [String].
  */
-public fun getRandomString(length: Int, charRange: CharRange): String =
-    CharArray(length) { charRange.random() }.concatToString()
+public fun getRandomString(length: Int, charRange: CharRange, random: Random = Random): String =
+    CharArray(length) { charRange.random(random) }.concatToString()
 
 /**
  * 根据所给 [charRanges] 随机生成长度为 [length] 的 [String].
  */
-public fun getRandomString(length: Int, vararg charRanges: CharRange): String =
-    CharArray(length) { charRanges[Random.Default.nextInt(0..charRanges.lastIndex)].random() }.concatToString()
+public fun getRandomString(length: Int, vararg charRanges: CharRange, random: Random = Random): String =
+    CharArray(length) { charRanges[random.nextInt(0..charRanges.lastIndex)].random(random) }.concatToString()
 
 
-public fun getRandomIntString(length: Int): String =
-    getRandomString(length, *intCharRanges)
+public fun getRandomIntString(length: Int, random: Random = Random): String =
+    getRandomString(length, *intCharRanges, random = random)
