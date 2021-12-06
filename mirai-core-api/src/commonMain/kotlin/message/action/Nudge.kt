e345e1dccf049cf18d0d2be7376933f35e6aba2f
Initial-heart <63091661+Initial-heart-1@users.noreply.github.com>
2021-12-06 22:06:42 +0800

    修改注释
    “安卓协议”的描述并不准确，ANDROID_PAD 也是安卓协议，但是完全不支持戳一戳
    修改进行明确

--- mirai-core-api/src/commonMain/kotlin/message/action/Nudge.kt
+++ mirai-core-api/src/commonMain/kotlin/message/action/Nudge.kt
@@ -42,7 +42,7 @@ public sealed class Nudge {
      *
      * @param receiver 这条 "戳一戳" 消息的接收对象. (不是 "戳" 动作的对象, 而是接收 "A 戳了 B" 这条消息的对象)
      * @return 成功发送时为 `true`. 若对方禁用 "戳一戳" 功能, 返回 `false`.
-     * @throws UnsupportedOperationException 当未使用 [安卓协议][MiraiProtocol.ANDROID_PHONE]或[IPAD协议][MiraiProtocol.IPAD] 时抛出
+     * @throws UnsupportedOperationException 当未使用 [ANDROID_PHONE协议][MiraiProtocol.ANDROID_PHONE]或[IPAD协议][MiraiProtocol.IPAD] 时抛出
      *
      * @see NudgeEvent 事件
      * @see Contact.sendNudge
@@ -61,7 +61,7 @@ public sealed class Nudge {
          *
          * @return 成功发送时为 `true`. 若对方禁用 "戳一戳" 功能, 返回 `false`.
          *
-         * @throws UnsupportedOperationException 当未使用 [安卓协议][MiraiProtocol.ANDROID_PHONE]或[IPAD协议][MiraiProtocol.IPAD] 时抛出
+         * @throws UnsupportedOperationException 当未使用 [ANDROID_PHONE协议][MiraiProtocol.ANDROID_PHONE]或[IPAD协议][MiraiProtocol.IPAD] 时抛出
          *
          * @see NudgeEvent 事件
          */
