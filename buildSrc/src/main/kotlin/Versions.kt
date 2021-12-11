e1c0024ba88c6f68c4ca9ae66b96c7e1c3b6e2b9
Him188 <Him188@mamoe.net>
2021-12-11 17:03:05 +0000

    Version 2.8.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -14,7 +14,7 @@ import org.gradle.kotlin.dsl.exclude
 import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 
 object Versions {
-    const val project = "2.8.2"
+    const val project = "2.8.3"
 
     const val core = project
     const val console = project
