41b424c555ccd1960c58dbd72558e367f0eb458f
Him188 <Him188@mamoe.net>
2021-12-26 17:41:04 +0000

    Remove MiraiConsoleBuildConstants from git and generate it everytime on build

--- mirai-console/backend/mirai-console/build.gradle.kts
+++ mirai-console/backend/mirai-console/build.gradle.kts
@@ -11,8 +11,8 @@
 
 import BinaryCompatibilityConfigurator.configureBinaryValidator
 import BinaryCompatibilityConfigurator.configureBinaryValidators
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.time.Instant
+import java.time.*
+import java.time.format.*
 
 plugins {
     kotlin("jvm")
@@ -53,28 +53,21 @@ dependencies {
 }
 
 tasks {
-    val compileKotlin by getting {}
-
-    register("fillBuildConstants") {
+    val task = register("generateBuildConstants") {
         group = "mirai"
         doLast {
-            (compileKotlin as KotlinCompile).source.filter { it.name == "MiraiConsoleBuildConstants.kt" }.single()
-                .let { file ->
-                    file.writeText(
-                        file.readText()
-                            .replace(
-                                Regex("""val buildDate: Instant = Instant.ofEpochSecond\(.*\)""")
-                            ) {
-                                """val buildDate: Instant = Instant.ofEpochSecond(${
-                                    Instant.now().epochSecond
-                                })"""
-                            }
-                            .replace(
-                                Regex("""const val versionConst:\s+String\s+=\s+".*"""")
-                            ) { """const val versionConst: String = "${project.version}"""" }
-                    )
-                }
+            val now = Instant.now()
+            project.file("src/internal/MiraiConsoleBuildConstants.kt").writeText(
+                project.file("src/internal/MiraiConsoleBuildConstants.kt.template").readText()
+                    .replace("GENERATION_DATE", now.atZone(ZoneId.systemDefault()).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                    .replace("BUILD_DATE", now.epochSecond.toString())
+                    .replace("VERSION_CONSTANT", project.version.toString())
+            )
         }
+    }
+
+    afterEvaluate {
+        getByName("compileKotlin").dependsOn(task)
     }
 }
 
