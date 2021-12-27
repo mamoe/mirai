86a389b585df96350407361fa79a5efa30773bab
Him188 <Him188@mamoe.net>
2021-12-27 18:25:49 +0000

    Set version to 2.10.0-RC

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -15,7 +15,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 
 object Versions {
     val project = System.getenv("mirai.build.project.version")
-        ?: /*PROJECT_VERSION_START*/"2.10.0-RC-null-1cd354a7"/*PROJECT_VERSION_END*/
+        ?: /*PROJECT_VERSION_START*/"2.10.0-RC"/*PROJECT_VERSION_END*/
 
     val core = project
     val console = project
