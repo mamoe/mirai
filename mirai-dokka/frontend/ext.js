5eea8ff90817bd1e592125e8a03ac5316313717e
purofle <3272912942@qq.com>
2022-01-02 23:16:16 +0800

    Update all copyright date

--- mirai-dokka/frontend/ext.js
+++ mirai-dokka/frontend/ext.js
@@ -1,7 +1,7 @@
 // noinspection ES6ConvertVarToLetConst,JSUnresolvedVariable
 
 /*
- * Copyright 2019-2021 Mamoe Technologies and contributors.
+ * Copyright 2019-2022 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
