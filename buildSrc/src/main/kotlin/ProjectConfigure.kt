4795839bfa354dcd476b91d582f027843354295f
Him188 <Him188@mamoe.net>
2022-02-07 16:23:50 +0000

    build: Remove unused experimentalAnnotations

--- buildSrc/src/main/kotlin/ProjectConfigure.kt
+++ buildSrc/src/main/kotlin/ProjectConfigure.kt
@@ -1,10 +1,10 @@
 /*
- * Copyright 2019-2021 Mamoe Technologies and contributors.
+ * Copyright 2019-2022 Mamoe Technologies and contributors.
  *
- *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
  *
- *  https://github.com/mamoe/mirai/blob/master/LICENSE
+ * https://github.com/mamoe/mirai/blob/dev/LICENSE
  */
 
 @file:Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
@@ -155,9 +155,7 @@ val experimentalAnnotations = arrayOf(
     "net.mamoe.mirai.utils.MiraiInternalApi",
     "net.mamoe.mirai.utils.MiraiExperimentalApi",
     "net.mamoe.mirai.LowLevelApi",
-    "net.mamoe.mirai.utils.UnstableExternalImage",
 
-    "net.mamoe.mirai.message.data.ExperimentalMessageKey",
     "net.mamoe.mirai.console.ConsoleFrontEndImplementation",
     "net.mamoe.mirai.console.util.ConsoleInternalApi",
     "net.mamoe.mirai.console.util.ConsoleExperimentalApi",
