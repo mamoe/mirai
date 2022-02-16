7dbdfa588a2b95904b304ff936a95b2401ef6f41
Him188 <Him188@mamoe.net>
2022-02-16 08:35:51 +0000

    Cancel console instance and join when JVM shutdown, if initialized

--- mirai-console/backend/mirai-console/src/command/BuiltInCommands.kt
+++ mirai-console/backend/mirai-console/src/command/BuiltInCommands.kt
@@ -1,10 +1,10 @@
 /*
- * Copyright 2019-2021 Mamoe Technologies and contributors.
+ * Copyright 2019-2022 Mamoe Technologies and contributors.
  *
- *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
  *
- *  https://github.com/mamoe/mirai/blob/master/LICENSE
+ * https://github.com/mamoe/mirai/blob/dev/LICENSE
  */
 
 package net.mamoe.mirai.console.command
@@ -53,7 +53,6 @@ import java.lang.management.ManagementFactory
 import java.lang.management.MemoryUsage
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
-import kotlin.concurrent.thread
 import kotlin.math.floor
 import kotlin.system.exitProcess
 
@@ -116,12 +115,6 @@ public object BuiltInCommands {
         public suspend fun CommandSender.handle() {
             sendMessage(generateDefaultHelp(this.permitteeId))
         }
-    }
-
-    init {
-        Runtime.getRuntime().addShutdownHook(thread(false) {
-            MiraiConsole.cancel()
-        })
     }
 
     public object StopCommand : SimpleCommand(
