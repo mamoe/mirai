bcf962b290277825e18e712ab47e40fbf00c4ef2
Karlatemp <karlatemp@vip.qq.com>
2022-02-18 00:25:46 +0800

    High-Speed logging test

--- mirai-console/backend/integration-test/test/testpoints/terminal/TestTerminalLogging.kt
+++ mirai-console/backend/integration-test/test/testpoints/terminal/TestTerminalLogging.kt
@@ -7,32 +7,80 @@
  * https://github.com/mamoe/mirai/blob/dev/LICENSE
  */
 
-@file:Suppress("INVISIBLE_MEMBER")
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
 
 package net.mamoe.console.integrationtest.testpoints.terminal
 
 import net.mamoe.console.integrationtest.AbstractTestPoint
 import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.MiraiConsoleImplementation
+import net.mamoe.mirai.console.terminal.LoggingServiceI
 import net.mamoe.mirai.console.terminal.MiraiConsoleImplementationTerminal
 import net.mamoe.mirai.utils.cast
 import net.mamoe.mirai.utils.info
 import java.io.File
 import java.util.*
+import kotlin.math.min
 import kotlin.test.assertTrue
+import kotlin.test.fail
 
 internal object TestTerminalLogging : AbstractTestPoint() {
+    override fun beforeConsoleStartup() {
+        System.setProperty("mirai.console.terminal.log.buffer", "10")
+    }
+
     override fun onConsoleStartSuccessfully() {
+        val logService = MiraiConsoleImplementation.getInstance()
+            .cast<MiraiConsoleImplementationTerminal>()
+            .logService.cast<LoggingServiceI>()
+
+        logService.autoSplitTask.cancel(true)
+        File("logs/log-0.log").delete()
+
         val stub = "Terminal Test: STUB" + UUID.randomUUID()
         MiraiConsole.mainLogger.info { stub }
+        Thread.sleep(200L)
+
         assertTrue { File("logs/latest.log").isFile }
         assertTrue { File("logs/latest.log").readText().contains(stub) }
-        MiraiConsoleImplementation.getInstance()
-            .cast<MiraiConsoleImplementationTerminal>()
-            .logService.switchLogFileNow.invoke()
+
+        logService.switchLogFileNow.invoke()
+
         assertTrue { File("logs/latest.log").isFile }
         assertTrue { !File("logs/latest.log").readText().contains(stub) }
         assertTrue { File("logs/log-0.log").isFile }
-        // Don't check content of "logs/log-0.log" because maybe daily auto split invoked
+        assertTrue { File("logs/log-0.log").readText().contains(stub) }
+
+        MiraiConsole.mainLogger.info("Pipeline size: " + logService.pipelineSize)
+
+        val logs = mutableListOf<String>()
+        logs.add("1================================================================")
+        repeat(100) { logs.add("TEST LINE $it -") }
+        logs.add("2================================================================")
+
+        logs.forEach { MiraiConsole.mainLogger.info(it) }
+
+        Thread.sleep(200L)
+
+        val lns = File("logs/latest.log").readLines().mapNotNull { line ->
+            logs.forEach { lx ->
+                if (line.contains(lx)) {
+                    return@mapNotNull lx
+                }
+            }
+            return@mapNotNull null
+        }
+
+        logService.switchLogFileNow.invoke()
+        // lns.forEach { println(it) }
+        var matched = 0
+        for (i in 0 until min(lns.size, logs.size)) {
+            if (lns[i] == logs[i]) matched++
+        }
+        println("Matched line: $matched, logs: ${logs.size}")
+        if (matched < (logs.size * 80 / 100)) {
+            lns.forEach { System.err.println(it) }
+            fail()
+        }
     }
 }
