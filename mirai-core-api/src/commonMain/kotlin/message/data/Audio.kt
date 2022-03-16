ab9c2ef2b2a3010c56b77d84d32fa53c6f565be9
Him188 <Him188@mamoe.net>
2022-03-16 15:33:56 +0000

    Add Audio.lengthDuration

--- mirai-core-api/src/commonMain/kotlin/message/data/Audio.kt
+++ mirai-core-api/src/commonMain/kotlin/message/data/Audio.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 Mamoe Technologies and contributors.
+ * Copyright 2019-2022 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -21,6 +21,8 @@ import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.message.MessageSerializers
 import net.mamoe.mirai.message.data.MessageChain.Companion.serializeToJsonString
 import net.mamoe.mirai.utils.*
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * 语音消息.
@@ -142,6 +144,14 @@ public interface OnlineAudio : Audio { // 协议实现
         public const val SERIAL_NAME: String = "OnlineAudio"
     }
 }
+
+/**
+ * 获取语音长度秒数, 作为 [Duration].
+ * @since 2.11
+ */
+@get:JvmSynthetic
+public inline val OnlineAudio.lengthDuration: Duration
+    get() = length.seconds
 
 /**
  * 离线语音消息.
