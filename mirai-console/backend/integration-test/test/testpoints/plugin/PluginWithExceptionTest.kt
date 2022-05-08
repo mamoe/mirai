3b4456b990a790c006da2c243abd9bb523bb45ff
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2022-05-08 18:44:04 +0800

    add PluginWithExceptionTest

---
+++ mirai-console/backend/integration-test/test/testpoints/plugin/PluginWithExceptionTest.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/mamoe/mirai/blob/dev/LICENSE
+ */
+
+package net.mamoe.console.integrationtest.testpoints.plugin
+
+import net.mamoe.console.integrationtest.AbstractTestPointAsPlugin
+import net.mamoe.mirai.console.extension.PluginComponentStorage
+import net.mamoe.mirai.console.plugin.PluginManager
+import net.mamoe.mirai.console.plugin.id
+import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
+import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import org.junit.jupiter.api.Assertions.assertFalse
+
+internal object PluginWithExceptionTest : AbstractTestPointAsPlugin() {
+
+    override fun newPluginDescription(): JvmPluginDescription {
+        return JvmPluginDescription(
+            id = "net.mamoe.testpoint.plugin-with-exception-test",
+            version = "1.0.0",
+            name = "PluginWithExceptionTest",
+        )
+    }
+
+    override fun beforeConsoleStartup() {
+        exceptedExceptionMessages = arrayOf("PluginWithExceptionTestExceptionTest")
+    }
+
+    override fun KotlinPlugin.onLoad0(storage: PluginComponentStorage) {
+
+    }
+
+    override fun KotlinPlugin.onEnable0() {
+        throw Exception("PluginWithExceptionTestExceptionTest")
+    }
+
+    override fun onConsoleStartSuccessfully() {
+        assertFalse { PluginManager.plugins.first{it.id == "net.mamoe.testpoint.plugin-with-exception-test"}.isEnabled }
+    }
+}
