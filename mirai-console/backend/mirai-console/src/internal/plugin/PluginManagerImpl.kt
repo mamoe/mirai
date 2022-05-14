07743af527b60e622f6614589cf9966536f12af3
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2022-05-14 15:29:27 +0800

    halt plugin enable process when mirai failed to enable it dependencies.

--- mirai-console/backend/mirai-console/src/internal/plugin/PluginManagerImpl.kt
+++ mirai-console/backend/mirai-console/src/internal/plugin/PluginManagerImpl.kt
@@ -17,16 +17,13 @@ import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.extensions.PluginLoaderProvider
 import net.mamoe.mirai.console.internal.data.mkdir
 import net.mamoe.mirai.console.internal.extension.GlobalComponentStorage
-import net.mamoe.mirai.console.plugin.Plugin
-import net.mamoe.mirai.console.plugin.PluginManager
+import net.mamoe.mirai.console.plugin.*
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.safeLoader
 import net.mamoe.mirai.console.plugin.description.PluginDependency
 import net.mamoe.mirai.console.plugin.description.PluginDescription
-import net.mamoe.mirai.console.plugin.id
 import net.mamoe.mirai.console.plugin.jvm.JvmPlugin
 import net.mamoe.mirai.console.plugin.loader.PluginLoadException
 import net.mamoe.mirai.console.plugin.loader.PluginLoader
-import net.mamoe.mirai.console.plugin.name
 import net.mamoe.mirai.console.util.SemVersion
 import net.mamoe.mirai.utils.TestOnly
 import net.mamoe.mirai.utils.cast
@@ -176,12 +173,17 @@ internal class PluginManagerImpl(
     }
 
     internal fun enableAllLoadedPlugins() {
-        resolvedPlugins.forEach {
-            try {
-                enablePlugin(it)
-            }catch (e:Exception){
-                logger.error(e)
-                logger.error("Cannot enable plugin ${it.id}")
+        resolvedPlugins.forEach { plugin ->
+            val tmp = plugin.dependencies.firstOrNull { d -> !PluginManager.plugins.first { it.id == d.id }.isEnabled }
+            if (tmp != null) {
+                logger.error("Cannot enable plugin ${plugin.id}, because mirai failed to enable ${tmp.id}")
+            } else {
+                try {
+                    enablePlugin(plugin)
+                } catch (e: Exception) {
+                    logger.error(e)
+                    logger.error("Cannot enable plugin ${plugin.id}")
+                }
             }
         }
     }
