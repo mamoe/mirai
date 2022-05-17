2cc814019df3d9068a86624a2c32afe091d5772b
Him188 <Him188@mamoe.net>
2022-05-17 09:43:09 +0100

    HMPP

--- mirai-core-utils/build.gradle.kts
+++ mirai-core-utils/build.gradle.kts
@@ -1,15 +1,13 @@
 /*
- * Copyright 2019-2021 Mamoe Technologies and contributors.
+ * Copyright 2019-2022 Mamoe Technologies and contributors.
  *
- *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
  *
- *  https://github.com/mamoe/mirai/blob/master/LICENSE
+ * https://github.com/mamoe/mirai/blob/dev/LICENSE
  */
 
 @file:Suppress("UNUSED_VARIABLE")
-
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 
 plugins {
     kotlin("multiplatform")
@@ -25,38 +23,31 @@ description = "mirai-core utilities"
 kotlin {
     explicitApi()
 
-    if (isAndroidSDKAvailable) {
-        jvm("android") {
-            attributes.attribute(KotlinPlatformType.attribute, KotlinPlatformType.androidJvm)
-            //   publishAllLibraryVariants()
-        }
-    } else {
-        printAndroidNotInstalled()
-    }
-
-    jvm("common") {
-        attributes.attribute(KotlinPlatformType.attribute, KotlinPlatformType.common)
-    }
-
-    jvm("jvm")
+    configureHMPPJvm()
 
     sourceSets {
         val commonMain by getting {
             dependencies {
                 api(kotlin("reflect"))
-                api(`kotlinx-serialization-core-jvm`)
-                api(`kotlinx-serialization-json-jvm`)
-                api(`kotlinx-coroutines-core-jvm`)
+                api(`kotlinx-serialization-core`)
+                api(`kotlinx-serialization-json`)
+                api(`kotlinx-coroutines-core`)
 
-                implementation(`kotlinx-atomicfu-jvm`)
-                implementation(`kotlinx-serialization-protobuf-jvm`)
-                implementationKotlinxIoJvm()
+                implementation(`kotlinx-atomicfu`)
+                implementation(`kotlinx-serialization-protobuf`)
+                implementationKotlinxIo(`kotlinx-io-common`)
             }
         }
 
         val commonTest by getting {
             dependencies {
-                api(`yamlkt-jvm`)
+                api(yamlkt)
+            }
+        }
+
+        val jvmBaseMain by getting {
+            dependencies {
+                implementationKotlinxIo(`kotlinx-io-jvm`)
             }
         }
 
@@ -75,6 +66,12 @@ kotlin {
         val jvmTest by getting {
             dependencies {
                 runtimeOnly(files("build/classes/kotlin/jvm/test")) // classpath is not properly set by IDE
+            }
+        }
+
+        val nativeMain by getting {
+            dependencies {
+                implementationKotlinxIo(`kotlinx-io-native`)
             }
         }
     }
