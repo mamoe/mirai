cd6b8a98c5356225babb5b53c7710dde8d767ac3
Him188 <Him188@mamoe.net>
2022-06-06 23:58:30 +0100

    Configure apiBuild dependsOn jar tasks only

--- buildSrc/src/main/kotlin/BinaryCompatibilityConfigurator.kt
+++ buildSrc/src/main/kotlin/BinaryCompatibilityConfigurator.kt
@@ -1,6 +1,14 @@
+/*
+ * Copyright 2019-2022 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/mamoe/mirai/blob/dev/LICENSE
+ */
+
 import org.gradle.api.Project
 import org.gradle.configurationcache.extensions.useToRun
-import org.gradle.kotlin.dsl.get
 import java.io.File
 
 /*
@@ -70,7 +78,11 @@ object BinaryCompatibilityConfigurator {
         project.afterEvaluate {
             findProject(getValidatorDir(dir))
                 ?.afterEvaluate {
-                    tasks.findByName("apiBuild")?.dependsOn(project.tasks["build"])
+                    if (targetName == null) {
+                        tasks.findByName("apiBuild")?.dependsOn(project.tasks.getByName("jar"))
+                    } else {
+                        tasks.findByName("apiBuild")?.dependsOn(project.tasks.getByName("${targetName}Jar"))
+                    }
                 }
         }
     }
