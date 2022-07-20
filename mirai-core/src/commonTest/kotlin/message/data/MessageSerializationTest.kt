a39cc2d1ff3a1bf3c3a478ea038d55cd3d1cc498
cssxsh <cssxsh@gmail.com>
2022-07-21 02:12:46 +0800

    fix: native test

--- mirai-core/src/commonTest/kotlin/message/data/MessageSerializationTest.kt
+++ mirai-core/src/commonTest/kotlin/message/data/MessageSerializationTest.kt
@@ -126,25 +126,23 @@ internal class MessageSerializationTest : AbstractTest() {
 
     @Test
     fun `test Image serialization`() {
-        val string = image.serialize<MessageContent>()
+        val string = image.serialize()
         val element = string.deserialize<JsonElement>()
         element as JsonObject
-        assertEquals(element["type"]?.jsonPrimitive?.content, "Image")
-        assertEquals(string.deserialize<MessageContent>(), image)
+        assertEquals(string.deserialize(), image)
 
         val image2 = Image(image.imageId) {
             type = ImageType.GIF
             width = 123
             height = 456
         }
-        val string2 = image2.serialize<MessageContent>()
+        val string2 = image2.serialize()
         val element2 = string2.deserialize<JsonElement>()
         element2 as JsonObject
-        assertEquals(element2["type"]?.jsonPrimitive?.content, "Image")
         assertEquals(element2["imageType"]?.jsonPrimitive?.content, image2.imageType.name)
         assertEquals(element2["width"]?.jsonPrimitive?.int, image2.width)
         assertEquals(element2["height"]?.jsonPrimitive?.int, image2.height)
-        val decoded = string2.deserialize<MessageContent>()
+        val decoded: Image = string2.deserialize()
         decoded as Image
         assertEquals(decoded.imageId, image2.imageId)
         assertEquals(decoded.imageType, image2.imageType)
@@ -153,18 +151,17 @@ internal class MessageSerializationTest : AbstractTest() {
 
         val string3 = """
         {
-            "type": "${Image.SERIAL_NAME}",
+            "imageType": "GIF",
             "width": 123,
             "height": 456,
             "imageId": "${image.imageId}"
          }
         """.trimIndent()
-        val decoded2 = string3.deserialize<MessageContent>()
-        decoded2 as Image
-        assertEquals(decoded.imageId, image2.imageId)
-        assertEquals(decoded.imageType, image2.imageType)
-        assertEquals(decoded.width, image2.width)
-        assertEquals(decoded.height, image2.height)
+        val decoded2: Image = string3.deserialize()
+        assertEquals(decoded2.imageId, image2.imageId)
+        assertEquals(decoded2.imageType, image2.imageType)
+        assertEquals(decoded2.width, image2.width)
+        assertEquals(decoded2.height, image2.height)
     }
 
     @Serializable
