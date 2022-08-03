0fe0e7afb85eb50b0813aaed3596040b79f78926
Him188 <Him188@mamoe.net>
2022-08-03 11:47:55 +0800

    [core] Fix `TestFactory` checks

--- mirai-core/src/jvmBaseTest/kotlin/test/AbstractTest.kt
+++ mirai-core/src/jvmBaseTest/kotlin/test/AbstractTest.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.debug.DebugProbes
 import net.mamoe.mirai.IMirai
 import net.mamoe.mirai.internal.network.framework.SynchronizedStdoutLogger
-import net.mamoe.mirai.internal.testFramework.DynamicTest
 import net.mamoe.mirai.internal.testFramework.TestFactory
 import net.mamoe.mirai.utils.MiraiLogger
 import net.mamoe.mirai.utils.setSystemProp
@@ -22,6 +21,7 @@ import org.junit.jupiter.api.TestInfo
 import org.junit.jupiter.api.Timeout
 import java.util.concurrent.TimeUnit
 import kotlin.jvm.optionals.getOrNull
+import kotlin.reflect.KClass
 import kotlin.reflect.full.functions
 import kotlin.reflect.full.hasAnnotation
 
@@ -39,7 +39,7 @@ internal actual abstract class AbstractTest actual constructor() : CommonAbstrac
                     check(function.returnType.classifier == List::class) {
                         "Illegal TestFactory function. A such function must return DynamicTestsResult."
                     }
-                    check(function.returnType.arguments.singleOrNull()?.type?.classifier == DynamicTest::class) {
+                    check((function.returnType.classifier as? KClass<*>)?.qualifiedName == List::class.qualifiedName) {
                         "Illegal TestFactory function. A such function must return DynamicTestsResult."
                     }
                 }
