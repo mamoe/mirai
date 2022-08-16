e99a0aebaf3d463b1be9d10bc7bcd35659e39ccf
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2022-08-16 08:02:34 -0400

    change friendGroupId type to Int?

--- mirai-core/src/commonMain/kotlin/network/protocol/packet/summarycard/SummaryCard.kt
+++ mirai-core/src/commonMain/kotlin/network/protocol/packet/summarycard/SummaryCard.kt
@@ -35,7 +35,7 @@ internal data class UserProfileImpl(
     override val qLevel: Int,
     override val sex: UserProfile.Sex,
     override val sign: String,
-    override val friendGroupId: Int,
+    override val friendGroupId: Int?,
 ) : Packet, UserProfile {
     override fun toString(): String {
         return "UserProfile(nickname=$nickname, email=$email, age=$age, qLevel=$qLevel, sex=$sex, sign=$sign, friendGroupId=$friendGroupId)"
