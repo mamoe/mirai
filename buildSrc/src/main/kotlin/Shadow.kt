cf132cecf8426fd97938a84f8930912b701ba7a4
Him188 <Him188@mamoe.net>
2022-10-03 12:44:41 +0100

    [build] Add notes to configureRelocationForCore

--- buildSrc/src/main/kotlin/Shadow.kt
+++ buildSrc/src/main/kotlin/Shadow.kt
@@ -263,6 +263,9 @@ private fun ShadowJar.setRelocations() {
 }
 
 fun Project.configureRelocationForCore() {
+    // WARNING: You must also consider relocating transitive dependencies.
+    // Otherwise, user will get NoClassDefFound error when using mirai as a classpath dependency. See #2263.
+
     relocateAllFromGroupId("io.ktor")
     relocateAllFromGroupId("com.squareup.okhttp3")
     relocateAllFromGroupId("com.squareup.okio")
