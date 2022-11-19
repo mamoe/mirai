9bc509fd686c66c90b19381e946c33b5fe2555c3
Him188 <Him188@mamoe.net>
2022-11-19 13:07:26 +0000

    fix task dependencies in publishing

--- buildSrc/src/main/kotlin/Shadow.kt
+++ buildSrc/src/main/kotlin/Shadow.kt
@@ -10,14 +10,14 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import org.gradle.api.GradleException
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Configuration
+import org.gradle.api.Task
 import org.gradle.api.publish.tasks.GenerateModuleMetadata
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.get
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
-import java.io.File
 
 /**
  * @see RelocationNotes
@@ -41,93 +41,46 @@ fun Project.configureMppShadow() {
  */
 private fun KotlinTarget.configureRelocationForTarget(project: Project) = project.run {
     val configuration = project.configurations.findByName(SHADOW_RELOCATION_CONFIGURATION_NAME)
-    val relocateDependencies =
-        // e.g. relocateJvmDependencies
-        tasks.create("relocate${targetName.titlecase()}Dependencies", ShadowJar::class) {
-            group = "mirai"
-            description = "Relocate dependencies to internal package"
-            destinationDirectory.set(buildDir.resolve("libs")) // build/libs
-            archiveBaseName.set("${project.name}-${targetName.toLowerCase()}") // e.g. "mirai-core-api-jvm"
 
-            dependsOn(compilations["main"].compileKotlinTask) // e.g. compileKotlinJvm
+    // e.g. relocateJvmDependencies
+    val relocateDependencies = tasks.create("relocate${targetName.titlecase()}Dependencies", ShadowJar::class) {
+        group = "mirai"
+        description = "Relocate dependencies to internal package"
+        destinationDirectory.set(buildDir.resolve("libs")) // build/libs
+        archiveBaseName.set("${project.name}-${targetName.toLowerCase()}") // e.g. "mirai-core-api-jvm"
 
-            from(compilations["main"].output) // Add compilation result of mirai sourcecode, not including dependencies
-            configuration?.let {
-                from(it) // Include runtime dependencies
-            }
+        dependsOn(compilations["main"].compileKotlinTask) // e.g. compileKotlinJvm
 
-            // Relocate packages
-            afterEvaluate {
-                val relocationFilters = project.relocationFilters
-                relocationFilters.forEach { relocation ->
-                    relocation.packages.forEach { aPackage ->
-                        relocate(aPackage, "$RELOCATION_ROOT_PACKAGE.$aPackage")
-                    }
+        from(compilations["main"].output) // Add compilation result of mirai sourcecode, not including dependencies
+        configuration?.let {
+            from(it) // Include runtime dependencies
+        }
+
+        // Relocate packages
+        afterEvaluate {
+            val relocationFilters = project.relocationFilters
+            relocationFilters.forEach { relocation ->
+                relocation.packages.forEach { aPackage ->
+                    relocate(aPackage, "$RELOCATION_ROOT_PACKAGE.$aPackage")
                 }
             }
         }
+    }
 
+    // Relocate before packing Jar. Projects that depends on this project actually (explicitly or implicitly) depends on the Jar.
+    tasks.getByName("${targetName}Jar").dependsOn(relocateDependencies)
 
-    // set publishing tasks depends on shadow
-    val allTasks = rootProject.allprojects.asSequence().flatMap { it.tasks }
-    allTasks
-        .filter {
-            it.name.startsWith("publish${targetName.titlecase()}PublicationTo")
-        }
-        .onEach { it.dependsOn(relocateDependencies) }
-        .count().let {
-            check(it > 0) { "[Shadow Relocation] Expected at least one publication matched for target $targetName." }
-        }
-
-
-    // Ensure all compilation has finished, otherwise Kotlin compiler will complain.
-    allTasks
-        .filter { it.name.endsWith("Jar") }
-        .onEach { relocateDependencies.dependsOn(it) }
-        .count().let {
-            check(it > 0) { "[Shadow Relocation] Expected at least one Jar task matched for target $targetName." }
-        }
-
-    allTasks
-        .filter { it.name.startsWith("compileKotlin") }
-        .onEach { relocateDependencies.dependsOn(it) }
-        .count().let {
-            check(it > 0) { "[Shadow Relocation] Expected at least one compileKotlin task matched for target $targetName." }
-        }
-
-    // We will modify metadata, so do generate metadata before relocation
-    val metadataTask =
+    // We will modify Kotlin metadata, so do generate metadata before relocation
+    val generateMetadataTask =
         tasks.getByName("generateMetadataFileFor${targetName.capitalize()}Publication") as GenerateModuleMetadata
-    relocateDependencies.dependsOn(metadataTask)
 
-    afterEvaluate {
-        // remove dependencies in Maven pom
-        mavenPublication {
-            pom.withXml {
-                val node = this.asNode().getSingleChild("dependencies")
-                val dependencies = node.childrenNodes()
-                logger.trace("[Shadow Relocation] deps: $dependencies")
-                dependencies.forEach { dep ->
-                    val groupId = dep.getSingleChild("groupId").value().toString()
-                    val artifactId = dep.getSingleChild("artifactId").value().toString()
-                    logger.trace("[Shadow Relocation] Checking $groupId:$artifactId")
-
-                    if (
-                        relocationFilters.any { filter ->
-                            filter.matchesDependency(groupId = groupId, artifactId = artifactId)
-                        }
-                    ) {
-                        logger.info("[Shadow Relocation] Filtering out '$groupId:$artifactId' from pom for project '${project.path}'")
-                        check(node.remove(dep)) { "Failed to remove dependency node" }
-                    }
-                }
-            }
-        }
+    val patchMetadataTask = tasks.create("patchMetadataFileFor${targetName.capitalize()}RelocatedPublication") {
+        dependsOn(generateMetadataTask)
 
         // remove dependencies in Kotlin module metadata
-        relocateDependencies.doLast {
+        doLast {
             // mirai-core-jvm-2.13.0.module
-            val file = metadataTask.outputFile.asFile.get()
+            val file = generateMetadataTask.outputFile.asFile.get()
             val metadata = Gson().fromJson(
                 file.readText(),
                 com.google.gson.JsonElement::class.java
@@ -159,6 +112,68 @@ private fun KotlinTarget.configureRelocationForTarget(project: Project) = projec
             file.writeText(GsonBuilder().setPrettyPrinting().create().toJson(metadata))
         }
     }
+
+    // Set "publishKotlinMultiplatformPublicationTo*" and "publish${targetName.capitalize()}PublicationTo*" dependsOn patchMetadataTask
+    if (project.kotlinMpp != null) {
+        tasks.filter { it.name.startsWith("publishKotlinMultiplatformPublicationTo") }.let { publishTasks ->
+            if (publishTasks.isEmpty()) {
+                throw GradleException("[Shadow Relocation] Cannot find publishKotlinMultiplatformPublicationTo for project '${project.path}'.")
+            }
+            publishTasks.forEach { it.dependsOn(patchMetadataTask) }
+        }
+
+        tasks.filter { it.name.startsWith("publish${targetName.capitalize()}PublicationTo") }.let { publishTasks ->
+            if (publishTasks.isEmpty()) {
+                throw GradleException("[Shadow Relocation] Cannot find publish${targetName.capitalize()}PublicationTo for project '${project.path}'.")
+            }
+            publishTasks.forEach { it.dependsOn(patchMetadataTask) }
+        }
+    }
+
+    afterEvaluate {
+        // Remove relocated dependencies in Maven pom
+        mavenPublication {
+            pom.withXml {
+                val node = this.asNode().getSingleChild("dependencies")
+                val dependencies = node.childrenNodes()
+                logger.trace("[Shadow Relocation] deps: $dependencies")
+                dependencies.forEach { dep ->
+                    val groupId = dep.getSingleChild("groupId").value().toString()
+                    val artifactId = dep.getSingleChild("artifactId").value().toString()
+                    logger.trace("[Shadow Relocation] Checking $groupId:$artifactId")
+
+                    if (
+                        relocationFilters.any { filter ->
+                            filter.matchesDependency(groupId = groupId, artifactId = artifactId)
+                        }
+                    ) {
+                        logger.info("[Shadow Relocation] Filtering out '$groupId:$artifactId' from pom for project '${project.path}'")
+                        check(node.remove(dep)) { "Failed to remove dependency node" }
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun Sequence<Task>.dependsOn(
+    relocateDependencies: ShadowJar,
+    kotlinTarget: KotlinTarget,
+): Int {
+    return onEach { relocateDependencies.dependsOn(it) }
+        .count().also {
+            check(it > 0) { "[Shadow Relocation] Expected at least one task task matched for target ${kotlinTarget.targetName}." }
+        }
+}
+
+private fun Sequence<Task>.mustRunAfter(
+    relocateDependencies: ShadowJar,
+    kotlinTarget: KotlinTarget,
+): Int {
+    return onEach { relocateDependencies.mustRunAfter(it) }
+        .count().also {
+            check(it > 0) { "[Shadow Relocation] Expected at least one task task matched for target ${kotlinTarget.targetName}." }
+        }
 }
 
 private fun Project.registerRegularShadowTask(target: KotlinTarget, mapTaskNameForMultipleTargets: Boolean): ShadowJar {
