a6af7e82eaecb5885258cca705a4d83da6354054
Him188 <Him188@mamoe.net>
2022-11-23 12:53:55 +0000

    [build] Fix check-publishing actions

--- ci-release-helper/build.gradle.kts
+++ ci-release-helper/build.gradle.kts
@@ -1,10 +1,10 @@
 /*
- * Copyright 2019-2021 Mamoe Technologies and contributors.
+ * Copyright 2019-2022 Mamoe Technologies and contributors.
  *
- *  此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- *  Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
  *
- *  https://github.com/mamoe/mirai/blob/master/LICENSE
+ * https://github.com/mamoe/mirai/blob/dev/LICENSE
  */
 import keys.SecretKeys
 import java.io.ByteArrayOutputStream
@@ -26,13 +26,7 @@ tasks.register("updateSnapshotVersion") {
     group = "mirai"
 
     doLast {
-        rootProject.file("buildSrc/src/main/kotlin/Versions.kt").run {
-            var text = readText()
-            val template = { version: Any? -> "/*PROJECT_VERSION_START*/\"${version}\"/*PROJECT_VERSION_END*/" }
-            check(text.indexOf(template(project.version)) != -1) { "Cannot find ${template(project.version)}" }
-            text = text.replace(template(project.version), template(snapshotVersion))
-            writeText(text)
-        }
+        setProjectVersionForFutureBuilds(snapshotVersion)
     }
 }
 
