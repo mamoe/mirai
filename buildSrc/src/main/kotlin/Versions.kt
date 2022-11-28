8f2f6197042502484050cdc125932697a26802fa
Karlatemp <kar@kasukusakura.com>
2022-11-28 10:06:32 +0800

    2.14.0-dev-publish-2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -23,7 +23,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 object Versions {
     val project = System.getenv("mirai.build.project.version")?.takeIf { it.isNotBlank() }
         ?: System.getProperty("mirai.build.project.version")?.takeIf { it.isNotBlank() }
-        ?: /*PROJECT_VERSION_START*/"2.14.0-dev-publish-1"/*PROJECT_VERSION_END*/
+        ?: /*PROJECT_VERSION_START*/"2.14.0-dev-publish-2"/*PROJECT_VERSION_END*/
     // DO NOT ADD SPACE BETWEEN!
 
     val core get() = project
