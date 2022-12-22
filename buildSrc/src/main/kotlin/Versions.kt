4545489eef1609d7dc2363a1d0bd5b4b70c0a068
Him188 <Him188@mamoe.net>
2022-12-22 12:58:35 +0000

    [idea] Bump IDEA and kotlin-idea version to 2022.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -35,7 +35,7 @@ object Versions {
     const val kotlinStdlib = kotlinCompiler
     const val dokka = "1.7.10"
 
-    const val kotlinCompilerForIdeaPlugin = "1.7.10"
+    const val kotlinCompilerForIdeaPlugin = "1.7.20"
 
     const val coroutines = "1.6.4"
     const val atomicFU = "0.18.3"
@@ -84,7 +84,7 @@ object Versions {
 
     // don't update easily unless you want your disk space -= 1000 MB
     // (700 MB for IDEA, 150 MB for sources, 150 MB for JBR)
-    const val intellij = "222.3345-EAP-CANDIDATE-SNAPSHOT"
+    const val intellij = "2022.3.1"
 }
 
 @Suppress("unused")
