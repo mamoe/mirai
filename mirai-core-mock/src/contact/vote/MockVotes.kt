dcc606e366e75f3ef2606a482ede3271d15e9c39
cssxsh <cssxsh@gmail.com>
2023-02-20 15:35:28 +0800

    feat: mock

--- mirai-core-mock/src/contact/vote/MockVotes.kt
+++ mirai-core-mock/src/contact/vote/MockVotes.kt
@@ -9,7 +9,71 @@
 
 package net.mamoe.mirai.mock.contact.vote
 
-import net.mamoe.mirai.contact.vote.Votes
+import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.contact.NormalMember
+import net.mamoe.mirai.contact.vote.*
+import net.mamoe.mirai.mock.MockBotDSL
+import net.mamoe.mirai.utils.MiraiInternalApi
 
 public interface MockVotes : Votes {
+
+    /**
+     * 直接以 [actor] 的身份推送一则投票
+     *
+     * @param events 当为 `true` 时会广播相关事件
+     * @param vote 见 [OfflineVote], [OfflineVote.create]
+     */
+    @MockBotDSL
+    public fun mockPublish(
+        vote: Vote,
+        actor: NormalMember,
+        events: Boolean
+    ): OnlineVote
+
+    @MockBotDSL
+    public fun mockPublish(
+        vote: Vote,
+        actor: NormalMember,
+    ): OnlineVote = mockPublish(vote, actor, false)
+
+
+    /**
+     * 直接以 [actor] 的身份做一次投票
+     *
+     * @param fid 见 [OnlineVote.fid]
+     */
+    @MockBotDSL
+    public fun mockPublish(
+        fid: String,
+        options: List<Int>,
+        actor: NormalMember
+    ): OnlineVoteRecord
+}
+
+public class MockOnlineVote @MiraiInternalApi public constructor(
+    override val senderId: Long,
+    override val fid: String,
+    override val publicationTime: Long,
+    override val select: MutableList<Int>,
+    override var records: List<OnlineVoteRecord>,
+    override val title: String,
+    override val options: List<String>,
+    override val parameters: VoteParameters
+) : OnlineVote {
+
+    override lateinit var group: Group
+
+    override val endTime: Long = publicationTime + parameters.end
+
+    override val sender: NormalMember? get() = group[senderId]
+}
+
+public class MockOnlineVoteRecordImpl @MiraiInternalApi public constructor(
+    override val vote: MockOnlineVote,
+    override val voterId: Long,
+    override val options: List<Int>,
+    override val time: Long
+) : OnlineVoteRecord {
+
+    override val voter: NormalMember? get() = vote.group[voterId]
 }
--- mirai-core-mock/src/internal/contact/MockVotesImpl.kt
+++ mirai-core-mock/src/internal/contact/MockVotesImpl.kt
@@ -10,34 +10,78 @@
 package net.mamoe.mirai.mock.internal.contact
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
 import net.mamoe.mirai.contact.Group
-import net.mamoe.mirai.contact.vote.OnlineVote
-import net.mamoe.mirai.contact.vote.OnlineVoteStatus
-import net.mamoe.mirai.contact.vote.Vote
-import net.mamoe.mirai.contact.vote.VoteImage
-import net.mamoe.mirai.mock.contact.vote.MockVotes
+import net.mamoe.mirai.contact.NormalMember
+import net.mamoe.mirai.contact.vote.*
+import net.mamoe.mirai.mock.contact.vote.*
+import net.mamoe.mirai.mock.utils.*
 import net.mamoe.mirai.utils.ExternalResource
+import net.mamoe.mirai.utils.currentTimeSeconds
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 
 internal class MockVotesImpl(
     val group: Group
 ) : MockVotes {
+
+    val votes = ConcurrentHashMap<String, MockOnlineVote>()
+
+    override fun mockPublish(vote: Vote, actor: NormalMember, events: Boolean): OnlineVote {
+        val mock = MockOnlineVote(
+            senderId = actor.id,
+            parameters = vote.parameters,
+            fid = UUID.randomUUID().toString(),
+            options = emptyList(),
+            select = MutableList(vote.options.size) { 0 },
+            records = emptyList(),
+            title = vote.title,
+            publicationTime = currentTimeSeconds()
+        )
+        mock.group = group
+        if (!events) return mock
+
+        // TODO: mirai-core no other events about votes
+        return mock
+    }
+
+    override fun mockPublish(fid: String, options: List<Int>, actor: NormalMember): OnlineVoteRecord {
+        val mock = votes[fid] ?: throw IllegalStateException("投票已删除")
+        val record = MockOnlineVoteRecordImpl(
+            vote = mock,
+            voterId = actor.id,
+            options = options,
+            time = currentTimeSeconds()
+        )
+        options.forEach { index ->
+            mock.select[index] = mock.select[index] + 1
+        }
+
+        return record
+    }
+
     override suspend fun publish(vote: Vote): OnlineVote {
-        TODO("Not yet implemented")
+        return mockPublish(vote = vote, actor = group.botAsMember, events = true)
     }
 
     override suspend fun uploadImage(resource: ExternalResource): VoteImage {
-        TODO("Not yet implemented")
+        return VoteImage.create(group.bot.mock().uploadMockImage(resource).imageId, 500, 500, "")
     }
 
     override suspend fun delete(fid: String): Boolean {
-        TODO("Not yet implemented")
+        return votes.remove(fid) != null
     }
 
-    override suspend fun get(fid: String): OnlineVoteStatus? {
-        TODO("Not yet implemented")
+    override suspend fun update(vote: OnlineVote) {
+        val mock = votes[vote.fid] ?: throw IllegalStateException("投票已删除")
+        // TODO
+    }
+
+    override suspend fun get(fid: String): OnlineVote? {
+        return votes[fid]
     }
 
     override fun asFlow(): Flow<Vote> {
-        TODO("Not yet implemented")
+        return votes.values.asFlow()
     }
 }
