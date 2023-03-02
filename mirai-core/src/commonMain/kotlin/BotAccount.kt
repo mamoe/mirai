d11614d792e1a0e634a566636204a1afd9566035
Him188 <Him188@mamoe.net>
2023-03-02 12:45:43 +0000

    Remove `phoneNumber` parameter from BotAccount

--- mirai-core/src/commonMain/kotlin/BotAccount.kt
+++ mirai-core/src/commonMain/kotlin/BotAccount.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 Mamoe Technologies and contributors.
+ * Copyright 2019-2023 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -17,11 +17,10 @@ import net.mamoe.mirai.utils.SecretsProtection
 internal class BotAccount(
     internal val id: Long,
     val authorization: BotAuthorization,
-    val phoneNumber: String = "",
 ) {
     constructor(
-        id: Long, pwd: String, phoneNumber: String = ""
-    ) : this(id, BotAuthorization.byPassword(pwd), phoneNumber)
+        id: Long, pwd: String
+    ) : this(id, BotAuthorization.byPassword(pwd))
 
     var accountSecretsKeyBuffer: SecretsProtection.EscapedByteBuffer? = null
 
--- mirai-core/src/commonMain/kotlin/network/protocol/packet/login/WtLogin.kt
+++ mirai-core/src/commonMain/kotlin/network/protocol/packet/login/WtLogin.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 Mamoe Technologies and contributors.
+ * Copyright 2019-2023 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -133,7 +133,8 @@ internal class WtLogin {
                         t142(client.apkId)
                         t145(client.device.guid)
                         t154(0)
-                        t112(client.account.phoneNumber.encodeToByteArray())
+                        // 需要 t112, 但在实现 QR 时删除了 phoneNumber
+//                        t112(client.account.phoneNumber.encodeToByteArray())
                         t116(client.miscBitMap, client.subSigMap)
                         t521()
                         t52c()
