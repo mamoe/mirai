c74e9d2858a85b95b45a7cf80ea678749c268aac
Him188 <Him188@mamoe.net>
2023-03-21 18:12:04 +0000

    [build] Make mirai-core `AbstractTest` public, and add `dependsOnCoreJvmTest` in buildSrc

--- mirai-core/src/nativeTest/kotlin/test/PlatformInitializationTest.kt
+++ mirai-core/src/nativeTest/kotlin/test/PlatformInitializationTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 Mamoe Technologies and contributors.
+ * Copyright 2019-2023 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -9,6 +9,8 @@
 
 package net.mamoe.mirai.internal.test
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.test.StandardTestDispatcher
 import net.mamoe.mirai.IMirai
 import net.mamoe.mirai.internal.initMirai
 import net.mamoe.mirai.utils.setSystemProp
@@ -31,7 +33,9 @@ internal actual class PlatformInitializationTest actual constructor() : Abstract
  * Note: To run a test in native sourceSets, use IDEA key shortcut 'control + shift + R' on macOS and 'Ctrl + Shift + R' on Windows.
  * Or you can right-click the function name of the test case and invoke 'Run ...'. You should not expect to see a button icon around the line numbers.
  */
-internal actual abstract class AbstractTest actual constructor() : CommonAbstractTest() {
+actual abstract class AbstractTest actual constructor() {
+    actual fun borrowSingleThreadDispatcher(): CoroutineDispatcher = StandardTestDispatcher()
+
     init {
         Companion
     }
