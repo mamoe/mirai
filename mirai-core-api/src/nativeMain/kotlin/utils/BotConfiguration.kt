403380514a7dac7dda008ee575fe022bf679ebd1
Him188 <Him188@mamoe.net>
2023-03-21 14:01:19 +0000

    [core] 更新 `MiraiProtocol` 注释, 注明 `ANDROID_WATCH` 不支持戳一戳, 删除 `ANDROID_PAD` 的错误注释 (#2572)

--- mirai-core-api/src/nativeMain/kotlin/utils/BotConfiguration.kt
+++ mirai-core-api/src/nativeMain/kotlin/utils/BotConfiguration.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 Mamoe Technologies and contributors.
+ * Copyright 2019-2023 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -225,13 +225,13 @@ public actual open class BotConfiguration { // open for Java
 
         /**
          * Android 平板.
-         *
-         * 注意: 不支持戳一戳事件解析
          */
         ANDROID_PAD,
 
         /**
          * Android 手表.
+         *
+         * 注意: 不支持戳一戳事件解析
          */
         ANDROID_WATCH,
 
