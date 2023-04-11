434db6e5b48890e245ef433f4ee1adb4ff01e353
StageGuard <1355416608@qq.com>
2023-04-11 08:48:56 +0800

    [core] fix base64 decode for android in rsa

--- mirai-core/src/androidMain/kotlin/utils/crypto/RSAAndroid.kt
+++ mirai-core/src/androidMain/kotlin/utils/crypto/RSAAndroid.kt
@@ -9,20 +9,19 @@
 
 package net.mamoe.mirai.internal.utils.crypto
 
+import android.util.Base64
 import java.security.KeyFactory
 import java.security.SecureRandom
 import java.security.spec.X509EncodedKeySpec
-import java.util.*
 import javax.crypto.Cipher
 
 internal actual fun rsaEncrypt(input: ByteArray, pemKey: String, seed: Long): ByteArray {
-    val encodedKey = Base64.getDecoder().decode(
-        pemKey
-            .removePrefix("-----BEGIN PUBLIC KEY-----")
-            .removeSuffix("-----END PUBLIC KEY-----")
-            .trim()
-            .replace("\n", "")
-    )
+    val encodedKey = pemKey
+        .removePrefix("-----BEGIN PUBLIC KEY-----")
+        .removeSuffix("-----END PUBLIC KEY-----")
+        .trim()
+        .replace("\n", "")
+        .let { Base64.decode(it, Base64.DEFAULT) }
 
     val keyFactory = KeyFactory.getInstance("RSA")
     val rsaPubKey = keyFactory.generatePublic(X509EncodedKeySpec(encodedKey))
