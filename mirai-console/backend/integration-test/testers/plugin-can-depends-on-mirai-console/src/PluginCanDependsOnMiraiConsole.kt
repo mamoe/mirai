b6e0f5e73da81110d1cf155867f8d64c9c305a57
Karlatemp <kar@kasukusakura.com>
2023-07-25 16:25:12 +0800

    [console/tests] Check plugin can depends on mirai-console

---
+++ mirai-console/backend/integration-test/testers/plugin-can-depends-on-mirai-console/src/PluginCanDependsOnMiraiConsole.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2019-2023 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/mamoe/mirai/blob/dev/LICENSE
+ */
+
+package net.mamoe.console.itest.plugincandependsonmiraiconsole
+
+import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
+import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+
+internal object PluginCanDependsOnMiraiConsole : KotlinPlugin(
+    JvmPluginDescription("net.mamoe.tester.plugin-can-depends-mirai-console", "1.0.0") {
+        dependsOn("net.mamoe.mirai-console")
+    }
+) {
+
+}
